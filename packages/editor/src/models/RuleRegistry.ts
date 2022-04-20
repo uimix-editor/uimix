d4b9e1bf324e945c0eea94f6a56055f31dae8516
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-20 19:34:41 +0900

    Implement ruleForID

--- packages/editor/src/models/RuleRegistry.ts
+++ packages/editor/src/models/RuleRegistry.ts
@@ -1,10 +1,25 @@
 import { observable } from "mobx";
+import * as CSSwhat from "css-what";
 import { Rule } from "./Rule";
 
 export class RuleRegistry {
   readonly rules = observable<Rule>([]);
 
   ruleForID(id: string): Rule | undefined {
-    throw new Error("TODO");
+    // TODO: create index
+
+    for (const rule of this.rules) {
+      if (rule.selector.length === 1 && rule.selector[0].length === 1) {
+        const selector = rule.selector[0][0];
+        if (
+          selector.type === "attribute" &&
+          selector.action === "equals" &&
+          selector.name === "id" &&
+          selector.value === id
+        ) {
+          return rule;
+        }
+      }
+    }
   }
 }
