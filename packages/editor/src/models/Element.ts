6a0ef37176f38273f8c6a9b83747eb30671b80a7
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-20 17:36:34 +0900

    Add more props to element

--- packages/editor/src/models/Element.ts
+++ packages/editor/src/models/Element.ts
@@ -2,12 +2,21 @@ import { TreeNode } from "@seanchas116/paintkit/dist/util/TreeNode";
 import { makeObservable, observable } from "mobx";
 import { Text } from "./Text";
 
+interface ElementOptions {
+  tagName: string;
+}
+
 export class Element extends TreeNode<Element, Element, Element | Text> {
-  constructor() {
+  constructor(options: ElementOptions) {
     super();
+    this.tagName = options.tagName;
     makeObservable(this);
   }
 
+  readonly tagName: string;
+
   // TODO: avoid id name confilict
   @observable id = "";
+
+  readonly attrs = observable.map<string, string>();
 }
