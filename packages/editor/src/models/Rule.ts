4b4c8b9e716909e0ddd291a85c776dc18b69a2a7
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-20 19:27:48 +0900

    Use css-what

--- packages/editor/src/models/Rule.ts
+++ packages/editor/src/models/Rule.ts
@@ -1,4 +1,5 @@
 import { makeObservable, observable } from "mobx";
+import * as CSSwhat from "css-what";
 import { Style } from "./Style";
 
 export class Rule {
@@ -9,6 +10,5 @@ export class Rule {
   readonly style = new Style();
 
   @observable mediaQuery = "";
-  // TODO: parse selector (use https://github.com/fb55/css-what ?)
-  @observable selector = "";
+  @observable selector: CSSwhat.Selector[][] = [];
 }
