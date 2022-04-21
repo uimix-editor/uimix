9c65336fb88ccabbf0fea50d917927cb2442d083
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 15:41:45 +0900

    Add RuleJSON and StyleJSON

--- packages/editor/src/models/Rule.ts
+++ packages/editor/src/models/Rule.ts
@@ -1,14 +1,33 @@
 import { makeObservable, observable } from "mobx";
 import * as CSSwhat from "css-what";
-import { Style } from "./Style";
+import { Style, StyleJSON } from "./Style";
+
+export interface RuleJSON {
+  mediaQuery: string;
+  selector: string;
+  style: StyleJSON;
+}
 
 export class Rule {
   constructor() {
     makeObservable(this);
   }
 
-  readonly style = new Style();
-
   @observable mediaQuery = "";
   @observable selector: CSSwhat.Selector[][] = [];
+  readonly style = new Style();
+
+  toJSON(): RuleJSON {
+    return {
+      mediaQuery: this.mediaQuery,
+      selector: CSSwhat.stringify(this.selector),
+      style: this.style.toJSON(),
+    };
+  }
+
+  loadJSON(json: RuleJSON): void {
+    this.mediaQuery = json.mediaQuery;
+    this.selector = CSSwhat.parse(json.selector);
+    this.style.loadJSON(json.style);
+  }
 }
