9b2daf7d33772f4ad1ec8091b22ea81839108fc0
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 15:58:58 +0900

    Add DocumentJSON

--- packages/editor/src/models/Document.ts
+++ packages/editor/src/models/Document.ts
@@ -1,6 +1,32 @@
 import { observable } from "mobx";
-import { Component } from "./Component";
+import { Component, ComponentJSON } from "./Component";
 
 export class Document {
   readonly components = observable<Component>([]);
+
+  toJSON(): DocumentJSON {
+    return {
+      components: this.components.map((component) => component.toJSON()),
+    };
+  }
+
+  loadJSON(json: DocumentJSON): void {
+    const oldComponents = new Map<string, Component>();
+    for (const component of this.components) {
+      oldComponents.set(component.key, component);
+    }
+
+    this.components.clear();
+    for (const componentJSON of json.components) {
+      const component =
+        oldComponents.get(componentJSON.key) ||
+        new Component(componentJSON.key);
+      component.loadJSON(componentJSON);
+      this.components.push(component);
+    }
+  }
+}
+
+export interface DocumentJSON {
+  components: ComponentJSON[];
 }
