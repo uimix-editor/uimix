7585a4812515dbc4793f823f0cae33731c15dc2c
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 16:23:45 +0900

    Add Editor

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  ColorSchemeProvider,
+  PaintkitProvider,
+} from "@seanchas116/paintkit/dist/components/GlobalStyle";
+import { Editor } from "./views/Editor";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <ColorSchemeProvider colorScheme="auto">
+      <PaintkitProvider>
+        <Editor />
+      </PaintkitProvider>
+    </ColorSchemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ packages/editor/src/views/Editor.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+import menuIcon from "@iconify-icons/ic/outline-menu";
+import paragraphIcon from "@seanchas116/paintkit/dist/icon/Paragraph";
+import frameIcon from "@seanchas116/paintkit/dist/icon/Frame";
+import { ToolBar } from "@seanchas116/paintkit/dist/components/toolbar/ToolBar";
+import {
+  ToolButton,
+  ToolButtonArray,
+} from "@seanchas116/paintkit/dist/components/toolbar/ToolButton";
+import { ZoomControl } from "@seanchas116/paintkit/dist/components/toolbar/ZoomControl";
+import { colors } from "@seanchas116/paintkit/dist/components/Palette";
+import { VSplitter } from "@seanchas116/paintkit/dist/components/sidebar/VSplitter";
+import {
+  InspectorTabBar,
+  InspectorTabBarItem,
+} from "@seanchas116/paintkit/dist/components/sidebar/InspectorTabBar";
+import { WidthResizeHandle } from "@seanchas116/paintkit/dist/components/sidebar/WidthResizeHandle";
+
+const Columns = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Viewport = styled.div`
+  background-color: ${colors.uiBackground};
+  flex: 1;
+`;
+
+const LeftSideBar = styled.div`
+  position: relative;
+  width: 200px;
+  border-right: 2px solid ${colors.separator};
+`;
+
+const RightSideBar = styled.div`
+  position: relative;
+  width: 200px;
+  border-left: 2px solid ${colors.separator};
+  > * {
+    height: 100%;
+  }
+`;
+
+export const Editor: React.FC = () => {
+  const [splitRatio, setSplitRatio] = React.useState(0.5);
+  const [leftWidth, setLeftWidth] = React.useState(200);
+  const [rightWidth, setRightWidth] = React.useState(200);
+
+  return (
+    <Columns>
+      <LeftSideBar
+        style={{
+          width: `${leftWidth}px`,
+        }}
+      >
+        <WidthResizeHandle
+          position="right"
+          width={leftWidth}
+          onChangeWidth={setLeftWidth}
+        />
+      </LeftSideBar>
+      <Center>
+        <ToolBar>
+          <ToolButton label="Menu" icon={menuIcon} />
+
+          <ToolButtonArray>
+            <ToolButton label="Frame" icon={frameIcon} />
+            <ToolButton label="Text" icon={paragraphIcon} />
+          </ToolButtonArray>
+
+          <ZoomControl
+            percentage={100}
+            onZoomIn={() => {}}
+            onZoomOut={() => {}}
+            onChangePercentage={() => {}}
+          />
+        </ToolBar>
+        <Viewport />
+      </Center>
+      <RightSideBar
+        style={{
+          width: `${rightWidth}px`,
+        }}
+      >
+        <VSplitter ratio={splitRatio} onChangeRatio={setSplitRatio}>
+          <div>
+            <InspectorTabBar>
+              <InspectorTabBarItem aria-selected>Tab 1</InspectorTabBarItem>
+              <InspectorTabBarItem>Tab 2</InspectorTabBarItem>
+            </InspectorTabBar>
+          </div>
+          <div>
+            <InspectorTabBar>
+              <InspectorTabBarItem aria-selected>Tab 1</InspectorTabBarItem>
+              <InspectorTabBarItem>Tab 2</InspectorTabBarItem>
+            </InspectorTabBar>
+          </div>
+        </VSplitter>
+        <WidthResizeHandle
+          position="left"
+          width={rightWidth}
+          onChangeWidth={setRightWidth}
+        />
+      </RightSideBar>
+    </Columns>
+  );
+};
