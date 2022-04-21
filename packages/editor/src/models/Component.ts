c3ddc51338f4810e1eb5e4571f20902f9d99bce1
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 15:49:11 +0900

    Add ComponentJSON

--- packages/editor/src/models/Component.ts
+++ packages/editor/src/models/Component.ts
@@ -1,6 +1,7 @@
 import { makeObservable, observable } from "mobx";
+import { ElementJSON } from "./Element";
 import { RootElement } from "./RootElement";
-import { Variant } from "./Variant";
+import { Variant, VariantJSON } from "./Variant";
 
 export class Component {
   constructor() {
@@ -13,4 +14,37 @@ export class Component {
   readonly variants = observable<Variant>([]);
 
   readonly rootElement = new RootElement(this);
+
+  toJSON(): ComponentJSON {
+    return {
+      name: this.name,
+      variants: this.variants.map((variant) => variant.toJSON()),
+      rootElement: this.rootElement.toJSON(),
+    };
+  }
+
+  loadJSON(json: ComponentJSON): void {
+    this.name = json.name;
+
+    const oldVariants = new Map<string, Variant>();
+    for (const variant of this.variants) {
+      oldVariants.set(variant.key, variant);
+    }
+
+    this.variants.clear();
+    for (const variantJSON of json.variants) {
+      const variant =
+        oldVariants.get(variantJSON.key) || new Variant(this, variantJSON.key);
+      variant.loadJSON(variantJSON);
+      this.variants.push(variant);
+    }
+
+    this.rootElement.loadJSON(json.rootElement);
+  }
+}
+
+export interface ComponentJSON {
+  name: string;
+  variants: VariantJSON[];
+  rootElement: ElementJSON;
 }
--- packages/editor/src/models/Variant.ts
+++ packages/editor/src/models/Variant.ts
@@ -1,22 +1,16 @@
 import { makeObservable, observable } from "mobx";
+import shortUUID from "short-uuid";
 import { Component } from "./Component";
 
-export interface VariantJSON {
-  selector?: string;
-  mediaQuery?: string;
-  x: number;
-  y: number;
-  width?: number;
-  height?: number;
-}
-
 export class Variant {
-  constructor(component: Component) {
+  constructor(component: Component, key?: string) {
     this.component = component;
+    this.key = key ?? shortUUID.generate();
     makeObservable(this);
   }
 
   readonly component: Component;
+  readonly key: string;
 
   @observable selector = "";
   @observable mediaQuery = "";
@@ -27,6 +21,7 @@ export class Variant {
 
   toJSON(): VariantJSON {
     return {
+      key: this.key,
       selector: this.selector || undefined,
       mediaQuery: this.mediaQuery || undefined,
       x: this.x,
@@ -37,6 +32,10 @@ export class Variant {
   }
 
   loadJSON(json: VariantJSON): void {
+    if (json.key !== this.key) {
+      throw new Error("Variant key mismatch");
+    }
+
     this.selector = json.selector || "";
     this.mediaQuery = json.mediaQuery || "";
     this.x = json.x;
@@ -44,4 +43,14 @@ export class Variant {
     this.width = json.width;
     this.height = json.height;
   }
+}
+
+export interface VariantJSON {
+  key: string;
+  selector?: string;
+  mediaQuery?: string;
+  x: number;
+  y: number;
+  width?: number;
+  height?: number;
 }
