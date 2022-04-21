e248cb6ad15370eae9de6798fc06ce0cf8eca64f
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 15:43:49 +0900

    Add VariantJSON

--- packages/editor/src/models/Variant.ts
+++ packages/editor/src/models/Variant.ts
@@ -1,6 +1,15 @@
 import { makeObservable, observable } from "mobx";
 import { Component } from "./Component";
 
+export interface VariantJSON {
+  selector?: string;
+  mediaQuery?: string;
+  x: number;
+  y: number;
+  width?: number;
+  height?: number;
+}
+
 export class Variant {
   constructor(component: Component) {
     this.component = component;
@@ -15,4 +24,24 @@ export class Variant {
   @observable y = 0;
   @observable width: number | undefined = undefined;
   @observable height: number | undefined = undefined;
+
+  toJSON(): VariantJSON {
+    return {
+      selector: this.selector || undefined,
+      mediaQuery: this.mediaQuery || undefined,
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+    };
+  }
+
+  loadJSON(json: VariantJSON): void {
+    this.selector = json.selector || "";
+    this.mediaQuery = json.mediaQuery || "";
+    this.x = json.x;
+    this.y = json.y;
+    this.width = json.width;
+    this.height = json.height;
+  }
 }
