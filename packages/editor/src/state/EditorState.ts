2f2cff856ad314d0f01fc863a1f90bae80bab4cf
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-21 16:11:55 +0900

    Add EditorState

---
+++ packages/editor/src/state/EditorState.ts
@@ -0,0 +1,10 @@
+import { JSONUndoHistory } from "@seanchas116/paintkit/dist/util/JSONUndoHistory";
+import { Document, DocumentJSON } from "../models/Document";
+
+export class EditorState {
+  constructor(history: JSONUndoHistory<DocumentJSON, Document>) {
+    this.history = history;
+  }
+
+  readonly history: JSONUndoHistory<DocumentJSON, Document>;
+}
