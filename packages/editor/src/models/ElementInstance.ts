742692e8e7987a1295882c39aa706e4dc96a583b
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-22 11:06:49 +0900

    Add selection

--- packages/editor/src/models/ElementInstance.ts
+++ packages/editor/src/models/ElementInstance.ts
@@ -1,3 +1,4 @@
+import { computed, makeObservable, observable } from "mobx";
 import { Element } from "./Element";
 import { TextInstance } from "./TextInstance";
 import { Variant } from "./Variant";
@@ -26,6 +27,7 @@ export class ElementInstance {
   private constructor(variant: Variant, element: Element) {
     this.variant = variant;
     this.element = element;
+    makeObservable(this);
   }
 
   get type(): "element" {
@@ -35,11 +37,54 @@ export class ElementInstance {
   readonly variant: Variant;
   readonly element: Element;
 
+  get parent(): ElementInstance | undefined {
+    return this.element.parent
+      ? ElementInstance.get(this.variant, this.element.parent)
+      : undefined;
+  }
+
   get children(): readonly (ElementInstance | TextInstance)[] {
     return this.element.children.map((child) =>
       child.type === "element"
         ? ElementInstance.get(this.variant, child)
         : TextInstance.get(this.variant, child)
     );
+  }
+
+  @observable selected = false;
+
+  @computed get ancestorSelected(): boolean {
+    return this.selected || this.parent?.ancestorSelected || false;
+  }
+
+  select(): void {
+    this.selected = true;
+    for (const child of this.children) {
+      child.deselect();
+    }
+  }
+
+  deselect(): void {
+    this.selected = false;
+    for (const child of this.children) {
+      child.deselect();
+    }
+  }
+
+  @computed.struct get selectedDescendants(): (
+    | ElementInstance
+    | TextInstance
+  )[] {
+    if (this.selected) {
+      return [this];
+    }
+    return this.children.flatMap((child) => child.selectedDescendants);
+  }
+
+  @observable collapsed = true;
+
+  expandAncestors(): void {
+    this.collapsed = false;
+    this.parent?.expandAncestors();
   }
 }
--- packages/editor/src/models/TextInstance.ts
+++ packages/editor/src/models/TextInstance.ts
@@ -1,3 +1,5 @@
+import { computed, makeObservable, observable } from "mobx";
+import { ElementInstance } from "./ElementInstance";
 import { Text } from "./Text";
 import { Variant } from "./Variant";
 
@@ -25,12 +27,37 @@ export class TextInstance {
   private constructor(variant: Variant, text: Text) {
     this.variant = variant;
     this.text = text;
+    makeObservable(this);
   }
 
   get type(): "text" {
     return "text";
   }
 
+  get parent(): ElementInstance | undefined {
+    return this.text.parent
+      ? ElementInstance.get(this.variant, this.text.parent)
+      : undefined;
+  }
+
   readonly variant: Variant;
   readonly text: Text;
+
+  @observable selected = false;
+
+  @computed get ancestorSelected(): boolean {
+    return this.selected || this.parent?.ancestorSelected || false;
+  }
+
+  select(): void {
+    this.selected = true;
+  }
+
+  deselect(): void {
+    this.selected = false;
+  }
+
+  @computed get selectedDescendants(): TextInstance[] {
+    return this.selected ? [this] : [];
+  }
 }
