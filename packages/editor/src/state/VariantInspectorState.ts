7a5124f6197d3551257edcca2e624c64aa3a8297
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-25 00:04:49 +0900

    Show variant selector edit

--- packages/editor/src/state/VariantInspectorState.ts
+++ packages/editor/src/state/VariantInspectorState.ts
@@ -1,4 +1,6 @@
-import { computed, makeObservable } from "mobx";
+import { filterInstance } from "@seanchas116/paintkit/src/util/Collection";
+import { MIXED, sameOrMixed } from "@seanchas116/paintkit/src/util/Mixed";
+import { action, computed, makeObservable } from "mobx";
 import { DefaultVariant, Variant } from "../models/Variant";
 import { EditorState } from "./EditorState";
 
@@ -10,11 +12,29 @@ export class VariantInspectorState {
 
   readonly editorState: EditorState;
 
-  @computed get selectedVariants(): (Variant | DefaultVariant)[] {
+  @computed get selectedAllVariants(): (Variant | DefaultVariant)[] {
     return this.editorState.document.selectedVariants;
   }
 
   @computed get isVisible(): boolean {
-    return this.selectedVariants.length > 0;
+    return this.selectedAllVariants.length > 0;
   }
+
+  @computed get selectedVariants(): Variant[] {
+    return filterInstance(this.editorState.document.selectedVariants, [
+      Variant,
+    ]);
+  }
+
+  @computed get selector(): string | typeof MIXED | undefined {
+    return sameOrMixed(this.selectedVariants.map((v) => v.selector));
+  }
+
+  readonly onChangeSelector = action((selector: string) => {
+    for (const v of this.selectedVariants) {
+      v.selector = selector;
+    }
+    this.editorState.history.commit("Change Variant Selector");
+    return true;
+  });
 }
--- packages/editor/src/views/VariantInspector.tsx
+++ packages/editor/src/views/VariantInspector.tsx
@@ -3,29 +3,31 @@ import React from "react";
 import styled from "styled-components";
 import {
   Pane,
+  PaneHeading,
+  PaneHeadingRow,
   TopLabelArea,
 } from "@seanchas116/paintkit/src/components/sidebar/Inspector";
-import { TextArea } from "@seanchas116/paintkit/src/components/TextArea";
 import { Label } from "@seanchas116/paintkit/src/components/Label";
-import { MIXED } from "@seanchas116/paintkit/src/util/Mixed";
+import { Input } from "@seanchas116/paintkit/src/components/Input";
 import { useEditorState } from "./EditorStateContext";
 
 const VariantInspectorWrap = styled.div``;
 
 export const VariantInspector: React.FC = observer(() => {
-  const state = useEditorState().elementInspectorState;
-  const { innerHTML } = state;
+  const state = useEditorState().variantInspectorState;
 
   return (
     <VariantInspectorWrap>
       <Pane>
-        <TopLabelArea>
-          <Label>Inner HTML</Label>
-          <TextArea
-            value={typeof innerHTML === "string" ? innerHTML : undefined}
-            placeholder={innerHTML === MIXED ? "Mixed" : undefined}
-          />
-        </TopLabelArea>
+        <PaneHeadingRow>
+          <PaneHeading>Variant</PaneHeading>
+        </PaneHeadingRow>
+        {state.selectedVariants.length > 0 && (
+          <TopLabelArea>
+            <Label>Selector</Label>
+            <Input value={state.selector} onChange={state.onChangeSelector} />
+          </TopLabelArea>
+        )}
       </Pane>
     </VariantInspectorWrap>
   );
