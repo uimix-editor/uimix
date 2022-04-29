79c2fb040796daf13e1adc4fba2b7102dccdbedc
Ryohei Ikegami <iofg2100@gmail.com>
2022-04-29 18:44:27 +0900

    WIP text pane

--- packages/editor/src/views/sidebar/inspector/StyleInspector.tsx
+++ packages/editor/src/views/sidebar/inspector/StyleInspector.tsx
@@ -1,8 +1,16 @@
 import { observer } from "mobx-react-lite";
 import React, { useMemo } from "react";
 import styled from "styled-components";
-import { useEditorState } from "../../EditorStateContext";
+import {
+  Pane,
+  PaneHeading,
+  Row11,
+  RowGroup,
+} from "@seanchas116/paintkit/src/components/sidebar/Inspector";
+import { ComboBox } from "@seanchas116/paintkit/src/components/ComboBox";
+import { Select } from "@seanchas116/paintkit/src/components/Select";
 import { StyleInspectorState } from "../../../state/StyleInspectorState";
+import { useEditorState } from "../../EditorStateContext";
 
 const StyleInspectorWrap = styled.div``;
 
@@ -14,5 +22,41 @@ export const StyleInspector: React.FC = observer(function StyleInspector() {
     [editorState]
   );
 
-  return <StyleInspectorWrap></StyleInspectorWrap>;
+  return (
+    <StyleInspectorWrap>
+      <Pane>
+        <PaneHeading>Text</PaneHeading>
+        <RowGroup>
+          <ComboBox
+            value={"Times"}
+            options={["Times", "Helvetica"].map((value) => ({ value }))}
+            onChange={(value) => {
+              // TODO
+              return false;
+            }}
+          />
+          <Row11>
+            <Select
+              value={"100"}
+              placeholder="Weight"
+              options={[
+                { value: "100", text: "Thin" },
+                { value: "200", text: "Extra Light" },
+                { value: "300", text: "Light" },
+                { value: "400", text: "Normal" },
+                { value: "500", text: "Medium" },
+                { value: "600", text: "Semi Bold" },
+                { value: "700", text: "Bold" },
+                { value: "800", text: "Extra Bold" },
+                { value: "900", text: "Black" },
+              ]}
+              onChange={(value: string) => {
+                // TODO
+              }}
+            />
+          </Row11>
+        </RowGroup>
+      </Pane>
+    </StyleInspectorWrap>
+  );
 });
