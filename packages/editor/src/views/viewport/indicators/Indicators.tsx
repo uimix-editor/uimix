98781a5cd4f3956cd48adec7c1552eb06e9415de
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-04 21:27:41 +0900

    Show indicators

--- packages/editor/src/views/viewport/indicators/Indicators.tsx
+++ packages/editor/src/views/viewport/indicators/Indicators.tsx
@@ -32,9 +32,52 @@ export const Indicators: React.VFC<{
     editorState.scroll.documentToViewport
   );
 
+  const dragPreviewRects = editorState.dragPreviewRects.map((rect) =>
+    rect.transform(editorState.scroll.documentToViewport)
+  );
+  const dropTargetPreviewRect = editorState.dropTargetPreviewRect?.transform(
+    editorState.scroll.documentToViewport
+  );
+  const dropIndexIndicator = editorState.dropIndexIndicator?.map((p) =>
+    p.transform(editorState.scroll.documentToViewport)
+  );
+
   return (
     <IndicatorsWrap className={className}>
       <IndicatorsSVG>
+        {dragPreviewRects.map((rect, i) => (
+          <rect
+            key={i}
+            x={rect.left}
+            y={rect.top}
+            width={rect.width}
+            height={rect.height}
+            fill="none"
+            strokeDasharray="2 2"
+            stroke={colors.active}
+          />
+        ))}
+        {dropTargetPreviewRect && (
+          <rect
+            x={dropTargetPreviewRect.left}
+            y={dropTargetPreviewRect.top}
+            width={dropTargetPreviewRect.width}
+            height={dropTargetPreviewRect.height}
+            fill="none"
+            strokeDasharray="2 2"
+            stroke={colors.active}
+          />
+        )}
+        {dropIndexIndicator && (
+          <line
+            x1={dropIndexIndicator[0].x}
+            y1={dropIndexIndicator[0].y}
+            x2={dropIndexIndicator[1].x}
+            y2={dropIndexIndicator[1].y}
+            strokeWidth={2}
+            stroke={colors.active}
+          />
+        )}
         {hoverRect && (
           <rect
             x={hoverRect.left}
