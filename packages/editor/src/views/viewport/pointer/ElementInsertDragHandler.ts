4457dacd74e0904ecd7a751c18f7596c7268a319
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-04 13:13:44 +0900

    Insert elements

--- packages/editor/src/views/viewport/pointer/ElementInsertDragHandler.ts
+++ packages/editor/src/views/viewport/pointer/ElementInsertDragHandler.ts
@@ -1,6 +1,7 @@
 import { assertNonNull } from "@seanchas116/paintkit/src/util/Assert";
 import { Rect, Vec2 } from "paintvec";
 import { Component } from "../../../models/Component";
+import { Element } from "../../../models/Element";
 import { ElementInstance } from "../../../models/ElementInstance";
 import { ElementPickResult } from "../../../mount/ElementPicker";
 import { EditorState } from "../../../state/EditorState";
@@ -9,27 +10,56 @@ import { DragHandler } from "./DragHandler";
 export class ElementInsertDragHandler implements DragHandler {
   constructor(editorState: EditorState, pickResult: ElementPickResult) {
     this.editorState = editorState;
-    this.initPos = editorState.scroll.documentPosForEvent(pickResult.event);
+    const documentPos = editorState.scroll.documentPosForEvent(
+      pickResult.event
+    );
 
-    this.component = new Component();
-    this.component.rename("my-component");
-    this.editorState.document.components.append(this.component);
+    const parent = pickResult.default;
+    if (!parent) {
+      this.component = new Component();
+      this.component.rename("my-component");
+      this.editorState.document.components.append(this.component);
+      this.instance = assertNonNull(this.component.defaultVariant.rootInstance);
+      this.initPos = documentPos;
 
-    this.instance = assertNonNull(this.component.defaultVariant.rootInstance);
+      this.component.defaultVariant.x = this.initPos.x;
+      this.component.defaultVariant.y = this.initPos.y;
+    } else {
+      const element = new Element({ tagName: "div" });
+      element.rename("div");
+      parent.element.append(element);
+      this.instance = assertNonNull(
+        ElementInstance.get(parent.variant, element)
+      );
+      this.parentOffset = parent.boundingBox.topLeft; // TODO: use offsetParent
+      this.initPos = documentPos.sub(this.parentOffset);
+      this.instance.style.position = "absolute";
+      this.instance.style.left = `${this.initPos.x}px`;
+      this.instance.style.top = `${this.initPos.y}px`;
+      this.instance.style.backgroundColor = "#cccccc";
+    }
 
-    this.component.defaultVariant.x = this.initPos.x;
-    this.component.defaultVariant.y = this.initPos.y;
+    this.editorState.document.deselect();
+    this.instance.select();
   }
 
   move(event: MouseEvent | DragEvent): void {
-    const pos = this.editorState.scroll.documentPosForEvent(event);
+    const pos = this.editorState.scroll
+      .documentPosForEvent(event)
+      .sub(this.parentOffset);
     const rect = Rect.boundingRect([pos, this.initPos]);
 
-    this.component.defaultVariant.x = rect.left;
-    this.component.defaultVariant.y = rect.top;
+    if (this.component) {
+      this.component.defaultVariant.x = rect.left;
+      this.component.defaultVariant.y = rect.top;
+    } else {
+      this.instance.style.left = `${rect.left}px`;
+      this.instance.style.top = `${rect.top}px`;
+    }
     this.instance.style.width = `${rect.width}px`;
     this.instance.style.height = `${rect.height}px`;
   }
+
   end(event: MouseEvent | DragEvent): void {
     // TODO
 
@@ -39,6 +69,7 @@ export class ElementInsertDragHandler implements DragHandler {
 
   private readonly editorState: EditorState;
   private readonly initPos: Vec2;
-  private readonly component: Component;
+  private readonly parentOffset = new Vec2();
+  private readonly component: Component | undefined;
   private readonly instance: ElementInstance;
 }
