622e995fe30fdeeb87d524b0a16912e97d6c277b
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-04 12:52:36 +0900

    Insert component

--- packages/editor/src/views/viewport/pointer/ElementInsertDragHandler.ts
+++ packages/editor/src/views/viewport/pointer/ElementInsertDragHandler.ts
@@ -1,16 +1,44 @@
+import { assertNonNull } from "@seanchas116/paintkit/src/util/Assert";
+import { Rect, Vec2 } from "paintvec";
+import { Component } from "../../../models/Component";
+import { ElementInstance } from "../../../models/ElementInstance";
 import { ElementPickResult } from "../../../mount/ElementPicker";
 import { EditorState } from "../../../state/EditorState";
 import { DragHandler } from "./DragHandler";
 
 export class ElementInsertDragHandler implements DragHandler {
   constructor(editorState: EditorState, pickResult: ElementPickResult) {
-    // TODO
+    this.editorState = editorState;
+    this.initPos = editorState.scroll.documentPosForEvent(pickResult.event);
+
+    this.component = new Component();
+    this.component.rename("my-component");
+    this.editorState.document.components.append(this.component);
+
+    this.instance = assertNonNull(this.component.defaultVariant.rootInstance);
+
+    this.component.defaultVariant.x = this.initPos.x;
+    this.component.defaultVariant.y = this.initPos.y;
   }
 
   move(event: MouseEvent | DragEvent): void {
-    // TODO
+    const pos = this.editorState.scroll.documentPosForEvent(event);
+    const rect = Rect.boundingRect([pos, this.initPos]);
+
+    this.component.defaultVariant.x = rect.left;
+    this.component.defaultVariant.y = rect.top;
+    this.component.defaultVariant.width = rect.width;
+    this.component.defaultVariant.height = rect.height;
   }
   end(event: MouseEvent | DragEvent): void {
     // TODO
+
+    this.editorState.insertMode = undefined;
+    this.editorState.history.commit("Insert component");
   }
+
+  private readonly editorState: EditorState;
+  private readonly initPos: Vec2;
+  private readonly component: Component;
+  private readonly instance: ElementInstance;
 }
