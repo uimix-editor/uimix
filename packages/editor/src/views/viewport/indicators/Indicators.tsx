bea9904a5b493876b132da9611f38434f2d4d648
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-04 11:41:42 +0900

    Show hover rects

--- packages/editor/src/views/viewport/indicators/Indicators.tsx
+++ packages/editor/src/views/viewport/indicators/Indicators.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { observer } from "mobx-react-lite";
+import { colors } from "@seanchas116/paintkit/src/components/Palette";
+import { useEditorState } from "../../EditorStateContext";
 import { ElementResizeBox } from "./ElementResizeBox";
 
 const IndicatorsWrap = styled.div`
@@ -23,9 +25,28 @@ const IndicatorsSVG = styled.svg`
 export const Indicators: React.VFC<{
   className?: string;
 }> = observer(function Indicators({ className }) {
+  const editorState = useEditorState();
+
+  const hoverRect =
+    editorState.hoveredItem?.type === "element"
+      ? editorState.hoveredItem.boundingBox.transform(
+          editorState.scroll.documentToViewport
+        )
+      : undefined;
+
   return (
     <IndicatorsWrap className={className}>
       <IndicatorsSVG>
+        {hoverRect && (
+          <rect
+            x={hoverRect.left}
+            y={hoverRect.top}
+            width={hoverRect.width}
+            height={hoverRect.height}
+            fill="none"
+            stroke={colors.active}
+          />
+        )}
         <ElementResizeBox />
       </IndicatorsSVG>
     </IndicatorsWrap>
