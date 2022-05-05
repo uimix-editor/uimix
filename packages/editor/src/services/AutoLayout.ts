14e8bf3ff4332c6d67cbceaa860e7b2046bc3cfd
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-05 22:52:05 +0900

    Rename

--- packages/editor/src/services/AutoLayout.ts
+++ packages/editor/src/services/AutoLayout.ts
@@ -6,42 +6,44 @@ import { ElementInstance } from "../models/ElementInstance";
 export type StackAlign = "start" | "center" | "end";
 
 export class AutoLayout {
-  static detectLayout(layers: readonly ElementInstance[]): {
-    layers: readonly ElementInstance[];
+  static detectLayout(elements: readonly ElementInstance[]): {
+    elements: readonly ElementInstance[];
     bbox: Rect;
     direction: "x" | "y";
     gap: number;
     align: StackAlign;
   } {
-    if (!layers.length) {
+    if (!elements.length) {
       return {
-        layers: layers,
+        elements,
         bbox: new Rect(),
         direction: "x",
         gap: 0,
         align: "start",
       };
     }
-    if (layers.length === 1) {
+    if (elements.length === 1) {
       return {
-        layers: layers,
-        bbox: layers[0].offsetBoundingBox,
+        elements,
+        bbox: elements[0].offsetBoundingBox,
         direction: "x",
         gap: 0,
         align: "start",
       };
     }
 
-    const topSorted = layers.slice();
+    const topSorted = elements.slice();
     topSorted.sort((a, b) => a.boundingBox.top - b.boundingBox.top);
-    const leftSorted = layers.slice();
+    const leftSorted = elements.slice();
     leftSorted.sort((a, b) => a.boundingBox.left - b.boundingBox.left);
-    const bbox = assertNonNull(Rect.union(...layers.map((o) => o.boundingBox)));
+    const bbox = assertNonNull(
+      Rect.union(...elements.map((o) => o.boundingBox))
+    );
 
     const xGaps: number[] = [];
     const yGaps: number[] = [];
 
-    for (let i = 1; i < layers.length; ++i) {
+    for (let i = 1; i < elements.length; ++i) {
       xGaps.push(
         leftSorted[i].boundingBox.left - leftSorted[i - 1].boundingBox.right
       );
@@ -53,12 +55,12 @@ export class AutoLayout {
     const direction = sum(yGaps) < sum(xGaps) ? "x" : "y";
 
     if (direction === "x") {
-      const startError = sum(layers.map((o) => o.boundingBox.top - bbox.top));
+      const startError = sum(elements.map((o) => o.boundingBox.top - bbox.top));
       const centerError = sum(
-        layers.map((o) => Math.abs(o.boundingBox.center.y - bbox.center.y))
+        elements.map((o) => Math.abs(o.boundingBox.center.y - bbox.center.y))
       );
       const endError = sum(
-        layers.map((o) => bbox.bottom - o.boundingBox.bottom)
+        elements.map((o) => bbox.bottom - o.boundingBox.bottom)
       );
       const align =
         startError < centerError && startError < endError
@@ -68,18 +70,22 @@ export class AutoLayout {
           : "end";
 
       return {
-        layers: leftSorted,
+        elements: leftSorted,
         bbox,
         direction: "x",
         gap: Math.max(Math.round(sum(xGaps) / xGaps.length), 0),
         align,
       };
     } else {
-      const startError = sum(layers.map((o) => o.boundingBox.left - bbox.left));
-      const centerError = sum(
-        layers.map((o) => Math.abs(o.boundingBox.center.x - bbox.center.x))
+      const startError = sum(
+        elements.map((o) => o.boundingBox.left - bbox.left)
       );
-      const endError = sum(layers.map((o) => bbox.right - o.boundingBox.right));
+      const centerError = sum(
+        elements.map((o) => Math.abs(o.boundingBox.center.x - bbox.center.x))
+      );
+      const endError = sum(
+        elements.map((o) => bbox.right - o.boundingBox.right)
+      );
       const align =
         startError < centerError && startError < endError
           ? "start"
@@ -88,7 +94,7 @@ export class AutoLayout {
           : "end";
 
       return {
-        layers: topSorted,
+        elements: topSorted,
         bbox,
         direction: "y",
         gap: Math.max(Math.round(sum(yGaps) / yGaps.length), 0),
