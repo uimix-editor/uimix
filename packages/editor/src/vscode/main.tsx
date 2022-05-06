74ecc54d36457f3f74ea1bd3101f96d9e15a1c32
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-07 00:45:21 +0900

    Expose comlink

--- packages/editor/src/vscode/main.tsx
+++ packages/editor/src/vscode/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Comlink from "comlink";
 import { VSCodeApp } from "./VSCodeApp";
 import { VSCodeFile } from "./VSCodeFile";
+import { API } from "./API";
+
+const vscode = acquireVsCodeApi();
 
 const file = new VSCodeFile();
+
+Comlink.expose(new API(file), {
+  addEventListener: window.addEventListener.bind(window),
+  removeEventListener: window.removeEventListener.bind(window),
+  postMessage: vscode.postMessage.bind(vscode),
+});
 
 // TODO: expose Comlink
 
