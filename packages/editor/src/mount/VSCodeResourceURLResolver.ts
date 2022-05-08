056815e233d9fdcb78f9be175839406f087c3787
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-09 01:10:51 +0900

    Use createObjectURL again

--- packages/editor/src/mount/VSCodeResourceURLResolver.ts
+++ packages/editor/src/mount/VSCodeResourceURLResolver.ts
@@ -1,6 +1,5 @@
-import { blobToDataURL } from "@seanchas116/paintkit/src/util/Blob";
-
-// VS Code resource URLs are not accessible from the iframe, so we need to fetch them manually and generate data URLs.
+// VS Code resource URLs are not accessible from the iframe, so we need to fetch them manually and generate object URLs.
+// TODO: revoke the object URL when the resource is no longer used
 export class VSCodeResourceURLResolver {
   private readonly urls = new Map<string, string>();
   private readonly pending = new Map<string, Promise<string>>();
@@ -22,7 +21,7 @@ export class VSCodeResourceURLResolver {
     const promise = (async () => {
       const response = await fetch(src);
       const blob = await response.blob();
-      const url = await blobToDataURL(blob);
+      const url = URL.createObjectURL(blob);
       this.urls.set(src, url);
       this.pending.delete(src);
       return url;
