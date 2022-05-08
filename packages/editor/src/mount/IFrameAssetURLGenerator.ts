2575afc35482086b694f33782915b1c150617f8d
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-08 23:12:58 +0900

    Cache efficiently

--- packages/editor/src/mount/IFrameAssetURLGenerator.ts
+++ packages/editor/src/mount/IFrameAssetURLGenerator.ts
@@ -3,19 +3,32 @@ export class IFrameAssetURLGenerator {
     return this.urls.get(url);
   }
 
-  readonly urls = new Map<string, string>();
+  private readonly urls = new Map<string, string>();
+  private readonly pending = new Map<string, Promise<string>>();
 
   async generate(src: string): Promise<string> {
-    if (this.urls.has(src)) {
-      return this.urls.get(src)!;
+    const cached = this.urls.get(src);
+    if (cached) {
+      return cached;
     }
-    console.log(src);
-    const response = await fetch(src);
-    const blob = await response.blob();
-    console.log(blob);
+    const pending = this.pending.get(src);
+    if (pending) {
+      return pending;
+    }
 
-    const url = URL.createObjectURL(blob);
-    this.urls.set(src, url);
-    return url;
+    const promise = (async () => {
+      console.log(src);
+      const response = await fetch(src);
+      const blob = await response.blob();
+      console.log(blob);
+
+      const url = URL.createObjectURL(blob);
+      this.urls.set(src, url);
+      this.pending.delete(src);
+      return url;
+    })();
+
+    this.pending.set(src, promise);
+    return promise;
   }
 }
