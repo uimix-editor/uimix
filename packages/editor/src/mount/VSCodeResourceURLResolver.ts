eb11d0db1d080bdef083e15399488a9c101a9b9d
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-09 18:52:37 +0900

    Refactor VSCodeResourceURLResolver using MobX

--- packages/editor/src/mount/VSCodeResourceURLResolver.ts
+++ packages/editor/src/mount/VSCodeResourceURLResolver.ts
@@ -1,10 +1,12 @@
+import { observable, runInAction } from "mobx";
+
 // VS Code resource URLs are not accessible from the iframe, so we need to fetch them manually and generate object URLs.
 // TODO: revoke the object URL when the resource is no longer used
 export class VSCodeResourceURLResolver {
-  private readonly urls = new Map<string, string>();
+  private readonly urls = observable.map<string, string>();
   private readonly pending = new Map<string, Promise<string>>();
 
-  async generateDataURL(src: string): Promise<string> {
+  resolve(src: string): string | undefined {
     if (!src.startsWith("https://file%2B.vscode-resource.vscode-cdn.net/")) {
       return src;
     }
@@ -15,19 +17,19 @@ export class VSCodeResourceURLResolver {
     }
     const pending = this.pending.get(src);
     if (pending) {
-      return pending;
+      return;
     }
 
     const promise = (async () => {
       const response = await fetch(src);
       const blob = await response.blob();
       const url = URL.createObjectURL(blob);
-      this.urls.set(src, url);
+      runInAction(() => {
+        this.urls.set(src, url);
+      });
       this.pending.delete(src);
       return url;
     })();
-
     this.pending.set(src, promise);
-    return promise;
   }
 }
