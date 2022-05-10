6f08ca27c1f0c9ac5f31e643e19840a83d51170a
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-10 22:10:44 +0900

    Emit class

--- packages/compiler/out_example.js
+++ packages/compiler/out_example.js
@@ -1,15 +1,15 @@
-const style = `
-  :host {
-    display: block;
-  }
-`;
-
-const template = `
-  <h1>Hello World</h1>
-`;
-
 export class MyComponent extends HTMLElement {
   constructor() {
+    const style = `
+      :host {
+        display: block;
+      }
+    `;
+
+    const template = `
+      <h1>Hello World</h1>
+    `;
+
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `<style>${style}</style>${template}`;
--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import * as prettier from "prettier";
+import { upperFirst, camelCase } from "lodash-es";
 import { parseHTMLFragment } from "./util";
 
 export function compileFile(filePath: string): void {
@@ -10,5 +12,33 @@ export function compileFile(filePath: string): void {
 export function compile(data: string): string {
   const ast = parseHTMLFragment(data);
   console.log(ast);
-  return "TODO";
+
+  const outputs: string[] = [];
+
+  for (const child of ast.children) {
+    if (child.type === "element" && child.tagName === "macaron-component") {
+      const name = child.properties?.name?.toString();
+      if (!name) {
+        throw new Error("macaron-component must have a name");
+      }
+
+      const className = upperFirst(camelCase(name));
+
+      outputs.push(`
+        class ${className} extends HTMLElement {
+          constructor() {
+            super();
+            this.attachShadow({ mode: "open" });
+            this.shadowRoot.innerHTML = ${"`<style>${style}</style>${template}`"};
+          }
+        }
+      `);
+    }
+  }
+
+  const output = outputs.join("");
+
+  return prettier.format(output, {
+    parser: "babel",
+  });
 }
