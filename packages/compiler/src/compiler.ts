8ecf6579884b660b4226d34102d83ee04c1a34cd
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-10 22:34:33 +0900

    Emit style and template

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -1,12 +1,51 @@
 import * as fs from "fs";
+import type * as hast from "hast";
 import * as prettier from "prettier";
 import { upperFirst, camelCase } from "lodash-es";
+import { toHtml } from "hast-util-to-html";
 import { parseHTMLFragment } from "./util";
 
 export function compileFile(filePath: string): void {
   const data = fs.readFileSync(filePath, "utf8");
   const out = compile(data);
   fs.writeFileSync(filePath.replace(/\.macaron$/, ".js"), out);
+}
+
+function compileComponent(ast: hast.Element): string {
+  const name = ast.properties?.name?.toString();
+  if (!name) {
+    throw new Error("macaron-component must have a name");
+  }
+
+  const className = upperFirst(camelCase(name));
+
+  let style: string = "";
+  let template: string = "";
+
+  for (const child of ast.children) {
+    if (child.type === "element" && child.tagName === "template") {
+      template = toHtml(child.content!);
+    }
+    if (child.type === "element" && child.tagName === "style") {
+      style = (child.children[0] as hast.Text).value;
+    }
+  }
+
+  return `
+    class ${className} extends HTMLElement {
+      constructor() {
+        super();
+
+        const style = ${JSON.stringify(style)};
+        const template = ${JSON.stringify(template)};
+
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML = ${"`<style>${style}</style>${template}`"};
+      }
+    }
+
+    customElements.define(${JSON.stringify(name)}, ${className});
+  `;
 }
 
 export function compile(data: string): string {
@@ -17,24 +56,7 @@ export function compile(data: string): string {
 
   for (const child of ast.children) {
     if (child.type === "element" && child.tagName === "macaron-component") {
-      const name = child.properties?.name?.toString();
-      if (!name) {
-        throw new Error("macaron-component must have a name");
-      }
-
-      const className = upperFirst(camelCase(name));
-
-      outputs.push(`
-        class ${className} extends HTMLElement {
-          constructor() {
-            super();
-            this.attachShadow({ mode: "open" });
-            this.shadowRoot.innerHTML = ${"`<style>${style}</style>${template}`"};
-          }
-        }
-
-        customElements.define(${JSON.stringify(name)}, ${className});
-      `);
+      outputs.push(compileComponent(child));
     }
   }
 
