cd1f533549e8a78c72e6ce5e254ca6c3eb04006a
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-10 13:24:04 +0900

    Cleanup

--- packages/editor/src/views/viewport/InnerHTMLEditor.tsx
+++ packages/editor/src/views/viewport/InnerHTMLEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect } from "react";
 import { popoverStyle } from "@seanchas116/paintkit/src/components/Common";
 import { colors } from "@seanchas116/paintkit/src/components/Palette";
 import { useViewModel } from "@seanchas116/paintkit/src/components/hooks/useViewModel";
