70eaa85a2caff63ca15f38f4826e4ffbf9627a23
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-13 15:47:14 +0900

    Add test for compiler CLI

---
+++ packages/compiler/src/cli.test.ts
@@ -0,0 +1,35 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+import tmp from "tmp-promise";
+import shell from "shelljs";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fixtureFilePath = path.resolve(
+  __dirname,
+  "../../test-project/test.macaron"
+);
+
+const cliFilePath = path.resolve(__dirname, "../bin/cli.js");
+
+describe("cli", () => {
+  let tmpDir: tmp.DirectoryResult;
+
+  beforeEach(async () => {
+    tmpDir = await tmp.dir();
+  });
+  afterEach(async () => {
+    await tmpDir.cleanup();
+  });
+
+  it("compiles", async () => {
+    const tmpDir = await tmp.dir();
+
+    shell.cp(fixtureFilePath, tmpDir.path);
+    shell.exec(`node ${cliFilePath} ${tmpDir.path}/test.macaron`);
+
+    const out = fs.readFileSync(`${tmpDir.path}/test.js`, "utf-8");
+    expect(out).toMatchSnapshot();
+  });
+});
