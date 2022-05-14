0034062eec65aa2cc0297ea68bb6762a1b0d4c9a
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-14 22:35:22 +0900

    Remove TODO

--- packages/editor/src/mount/ComponentMount.ts
+++ packages/editor/src/mount/ComponentMount.ts
@@ -130,7 +130,6 @@ export class ComponentMount {
 
         for (const node of props.nodes) {
           if (node.type === "decl" && node.prop === "background") {
-            // TODO: resolve URL
             // eslint-disable-next-line
             node.value = replaceCSSURL(node.value, (url: string) =>
               this.context.editorState.resolveImageAssetURL(url)
