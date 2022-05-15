7ff39cff6cee8e8cb15ab5d12e5ee916e597a3ab
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-15 18:34:43 +0900

    Add shoelace loader js

---
+++ packages/test-project/shoelace.js
@@ -0,0 +1,7 @@
+import "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.73/dist/shoelace.js";
+
+const cssLink = document.createElement("link");
+cssLink.rel = "stylesheet";
+cssLink.href =
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.73/dist/themes/light.css";
+document.head.appendChild(cssLink);
