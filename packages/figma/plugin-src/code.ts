b5de8417fed2c5f68fed3caa5d6e41a01e29580f
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-20 14:20:57 +0900

    Node to macaron

--- packages/figma/plugin-src/code.ts
+++ packages/figma/plugin-src/code.ts
@@ -1,20 +1,22 @@
+import { figmaToMacaron } from "./traverse";
+import { compact } from "lodash-es";
+
 figma.showUI(__html__, { themeColors: true, height: 300 });
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === "create-rectangles") {
-    const nodes = [];
-
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === "copy") {
+    if (figma.currentPage.selection.length === 0) {
+      figma.notify("Select a layer to copy");
+      return;
     }
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    const macaronLayers = compact(
+      await Promise.all(
+        figma.currentPage.selection.map((node) =>
+          figmaToMacaron(node, { x: 0, y: 0 })
+        )
+      )
+    );
+    console.log(macaronLayers);
   }
-
-  figma.closePlugin();
 };
--- packages/figma/ui-src/App.tsx
+++ packages/figma/ui-src/App.tsx
@@ -5,39 +5,19 @@ import Logo from "./Logo";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const onCreate = () => {
-    const count = Number(inputRef.current?.value || 0);
-    parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", count } },
-      "*"
-    );
-  };
-
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  const onCopy = () => {
+    parent.postMessage({ pluginMessage: { type: "copy" } }, "*");
   };
 
   return (
     <main>
       <header>
-        <img src={logoPng} />
-        &nbsp;
-        <img src={`data:image/svg+xml;utf8,${logoSvg}`} />
-        &nbsp;
-        <Logo />
-        <h2>Rectangle Creator</h2>
+        <h2>Figma to Macaron</h2>
       </header>
-      <section>
-        <input id="input" type="number" min="0" ref={inputRef} />
-        <label htmlFor="input">Rectangle Count</label>
-      </section>
       <footer>
-        <button className="brand" onClick={onCreate}>
-          Create
+        <button className="brand" onClick={onCopy}>
+          Copy
         </button>
-        <button onClick={onCancel}>Cancel</button>
       </footer>
     </main>
   );
