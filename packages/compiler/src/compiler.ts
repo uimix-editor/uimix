f53b255ecc909f37c8d404bdec41120893ebac04
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-22 12:28:32 +0900

    Include reset CSS in JS output

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -4,8 +4,10 @@ import type * as hast from "hast";
 import * as prettier from "prettier";
 import { upperFirst, camelCase } from "lodash-es";
 import { toHtml } from "hast-util-to-html";
+import dedent from "dedent";
 import { parseHTMLFragment } from "./util";
 import { fixAssetPathInCSS, fixAssetPathInHTMLTree } from "./fix-asset-path";
+import { resetCSS } from "./resetCSS";
 
 export function compileFile(filePath: string, outputDir?: string): void {
   const data = fs.readFileSync(filePath, "utf8");
@@ -48,7 +50,7 @@ function compileComponent(ast: hast.Element): string {
       constructor() {
         super();
 
-        const style = \`${style}\`;
+        const style = \`${resetCSS}\n${dedent(style)}\`;
         const template = \`${template}\`;
 
         this.attachShadow({ mode: "open" });
