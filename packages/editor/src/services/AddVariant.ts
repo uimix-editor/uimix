abaf9e1ab04d561ace1759ae9880e7982534d4ae
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-26 20:28:18 +0900

    Better position

--- packages/editor/src/services/AddVariant.ts
+++ packages/editor/src/services/AddVariant.ts
@@ -1,9 +1,19 @@
+import { last } from "lodash-es";
 import { Component } from "../models/Component";
 import { Variant } from "../models/Variant";
 
 export function addVariant(component: Component): Variant {
+  const lastVariant = last(component.allVariants);
+  const lastVariantRect = lastVariant?.rootInstance?.boundingBox;
+
   const variant = new Variant();
   variant.selector = ":hover";
+
+  if (lastVariantRect) {
+    variant.x = lastVariantRect.left + lastVariantRect.width + 24;
+    variant.y = lastVariantRect.top;
+  }
+
   component.variants.append(variant);
 
   return variant;
