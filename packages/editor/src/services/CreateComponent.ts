560bfb97a2e4fc8154a30bcd336e5b0aae1b393b
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-27 19:23:09 +0900

    Move positional CSS properties to root element

--- packages/editor/src/services/CreateComponent.ts
+++ packages/editor/src/services/CreateComponent.ts
@@ -1,6 +1,18 @@
 import { Component } from "../models/Component";
 import { ElementInstance } from "../models/ElementInstance";
 
+const positionalProperties = [
+  "position",
+  "top",
+  "right",
+  "bottom",
+  "left",
+  "marginTop",
+  "marginRight",
+  "marginBottom",
+  "marginLeft",
+] as const;
+
 export function createComponent(instance: ElementInstance): Component {
   const document = instance.element.component?.document;
   if (!document) {
@@ -13,6 +25,17 @@ export function createComponent(instance: ElementInstance): Component {
   document.components.append(component);
 
   component.defaultVariant.rootInstance.setInnerHTML([html]);
+
+  for (const property of positionalProperties) {
+    component.defaultVariant.rootInstance.style[property] =
+      instance.style[property];
+
+    for (const child of component.defaultVariant.rootInstance.children) {
+      if (child.type === "element") {
+        child.style[property] = undefined;
+      }
+    }
+  }
 
   return component;
 }
