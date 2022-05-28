b7ab1bdd13b6d4d453be68a856607fb0a1d7d8aa
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-28 13:29:35 +0900

    Add test

---
+++ packages/editor/src/models/Document.test.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import fs from "fs";
+import url from "url";
+import { describe, it, expect, beforeEach } from "vitest";
+import { parseDocument, stringifyDocument } from "../fileFormat/document";
+import { Document } from "./Document";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const fixtureFilePath = path.resolve(
+  __filename,
+  "../../../../test-project/test.macaron"
+);
+const data = fs.readFileSync(fixtureFilePath, "utf-8");
+
+let doc: Document;
+
+beforeEach(() => {
+  doc = parseDocument(data);
+});
+
+describe(Document.name, () => {
+  describe(Document.prototype.renameCSSVariableUsages.name, () => {
+    it("should rename CSS variable usages", () => {
+      doc.renameCSSVariableUsages(
+        "--user-card-name-color",
+        "--user-card-name-color-new"
+      );
+
+      const saved = stringifyDocument(doc);
+      expect(saved).toContain("--user-card-name-color-new");
+      expect(saved).toMatchSnapshot();
+    });
+  });
+});
