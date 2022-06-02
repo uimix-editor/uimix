2e4c8d552bae1605429212432929c7ebbf886cac
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-03 00:56:44 +0900

    Load macaron-editor in top page

--- packages/docs/TopWrap.vue
+++ packages/docs/TopWrap.vue
@@ -1,11 +1,13 @@
 <template>
   <top-page></top-page>
+  <macaron-editor></macaron-editor>
 </template>
 
 <script>
 export default {
   mounted() {
     import('./components.macaron')
+    import('@macaron-app/editor/dist/webcomponent/main')
   }
 }
 </script>
