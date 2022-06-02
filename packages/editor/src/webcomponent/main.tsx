5c9609a27029ccbc16151d50d7117b99ef2af97e
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-02 22:24:39 +0900

    Add webcomponent

---
+++ packages/editor/src/webcomponent/main.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ColorSchemeProvider,
+  PaintkitProvider,
+} from "@seanchas116/paintkit/src/components/GlobalStyle";
+import { ContextMenuProvider } from "@seanchas116/paintkit/src/components/menu/ContextMenuProvider";
+import { JSONUndoHistory } from "@seanchas116/paintkit/src/util/JSONUndoHistory";
+import { DocumentJSON, Document } from "../models/Document";
+import { EditorState } from "../state/EditorState";
+
+class EditorElementEditorState extends EditorState {
+  readonly history = new JSONUndoHistory<DocumentJSON, Document>(
+    new Document()
+  );
+}
+
+const App: React.FC<{
+  editorState: EditorState;
+}> = ({ editorState }) => {
+  return (
+    <ColorSchemeProvider colorScheme="auto">
+      <GlobalStyle />
+      <PaintkitProvider>
+        <ContextMenuProvider>
+          <Editor editorState={editorState} />
+        </ContextMenuProvider>
+      </PaintkitProvider>
+    </ColorSchemeProvider>
+  );
+};
+
+export class MacaronEditorElement extends HTMLElement {
+  private _editorState: EditorElementEditorState;
+
+  constructor() {
+    super();
+    this._editorState = new EditorElementEditorState();
+  }
+
+  connectedCallback(): void {
+    const mountPoint = document.createElement("span");
+    this.attachShadow({ mode: "open" }).appendChild(mountPoint);
+
+    const root = ReactDOM.createRoot(mountPoint);
+    root.render(
+      <React.StrictMode>
+        <App editorState={this._editorState} />
+      </React.StrictMode>
+    );
+  }
+
+  get editorState(): EditorElementEditorState {
+    return this._editorState;
+  }
+}
