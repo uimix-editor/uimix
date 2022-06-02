69a795f31cdc7e3f8b5b48cb6a10077266860a6d
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-03 01:06:27 +0900

    Tweak

--- packages/docs/TopWrap.vue
+++ packages/docs/TopWrap.vue
@@ -1,7 +1,13 @@
 <style scoped>
-  macaron-editor {
-    height: 640px;
-  }
+macaron-editor {
+  height: 640px;
+
+  border: 0.5px solid rgba(255, 255, 255, 0.12);
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15),
+    0px 24px 30px rgba(0, 0, 0, 0.35);
+  border-radius: 10px;
+  overflow: hidden;
+}
 </style>
 
 <template>
@@ -12,9 +18,9 @@
 <script>
 export default {
   mounted() {
-    import('./components.macaron')
-    import('@macaron-app/editor/dist/webcomponent/main.js')
-    import('@macaron-app/editor/dist/webcomponent/main.css')
-  }
-}
+    import("./components.macaron");
+    import("@macaron-app/editor/dist/webcomponent/main.js");
+    import("@macaron-app/editor/dist/webcomponent/main.css");
+  },
+};
 </script>
