c4b03c27d9ccb23394c9b462c22b4ca6194f946d
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-03 20:19:09 +0900

    Fix

--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
-import "@fontsource/readex-pro/variable.css";
+import "@fontsource/readex-pro/400.css";
+import "@fontsource/readex-pro/700.css";
 import "./custom.css";
 
 export default DefaultTheme;
