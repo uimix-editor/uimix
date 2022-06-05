1c255c9271e1bc2f6752e294f9ef20af5381fdf7
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-05 23:55:06 +0900

    Tweak height

--- packages/docs/Playground.vue
+++ packages/docs/Playground.vue
@@ -5,7 +5,7 @@ demo-tab {
 
 .playground-floating {
   width: 100%;
-  height: 640px;
+  height: 480px;
   border: 0.5px solid rgba(255, 255, 255, 0.12);
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15),
     0px 24px 30px rgba(0, 0, 0, 0.35);
