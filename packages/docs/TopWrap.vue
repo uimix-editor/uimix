49e612d8311cd52772707e78083b15c4ea56284c
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-05 18:05:41 +0900

    Monaco editor

--- packages/docs/TopWrap.vue
+++ packages/docs/TopWrap.vue
@@ -17,11 +17,12 @@ top-page {
   position: relative;
 }
 
-macaron-editor {
-  width: calc(100% - 64px);
-  max-width: 1280px;
-  height: 640px;
+demo-tab {
+  cursor: pointer;
+}
 
+.playground-floating {
+  height: 640px;
   border: 0.5px solid rgba(255, 255, 255, 0.12);
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.15),
     0px 24px 30px rgba(0, 0, 0, 0.35);
@@ -29,8 +30,22 @@ macaron-editor {
   overflow: hidden;
 }
 
-demo-tab {
-  cursor: pointer;
+.playground {
+  width: calc(100% - 64px);
+  max-width: 1280px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 16px;
+}
+
+.html-editor-preview {
+  position: relative;
+  background-color: white;
+}
+
+.html-editor {
+  height: 100%;
 }
 </style>
 
@@ -45,14 +60,21 @@ demo-tab {
     >
       {{ demoFile.name }}
     </demo-tab>
-    <macaron-editor
-      slot="demo-editor"
-      :value="currentDemoFile.content"
-    ></macaron-editor>
+    <div slot="demo-editor" class="playground">
+      <macaron-editor
+        class="playground-floating"
+        slot="demo-editor"
+        :value="currentDemoFile.content"
+      ></macaron-editor>
+      <div class="playground-floating html-editor-preview">
+        <div ref="editorBody" class="html-editor"></div>
+      </div>
+    </div>
   </top-page>
 </template>
 
 <script>
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import basicMacaronFile from "./examples/basic.macaron?raw";
 import interactionsMacaronFile from "./examples/interactions.macaron?raw";
 
@@ -89,6 +111,14 @@ export default {
     import("./components.macaron");
     import("@macaron-app/editor/dist/webcomponent/main.js");
     import("@macaron-app/editor/dist/webcomponent/main.css");
+
+    const editorBody = this.$refs.editorBody;
+    monaco.editor.create(editorBody, {
+      value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join(
+        "\n"
+      ),
+      language: "typescript",
+    });
   },
 };
 </script>
