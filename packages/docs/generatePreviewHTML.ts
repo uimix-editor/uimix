a19296d916b873b2b3d6f320404a5c3436867eef
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-06 00:38:52 +0900

    Load google fonts

--- packages/docs/generatePreviewHTML.ts
+++ packages/docs/generatePreviewHTML.ts
@@ -9,9 +9,11 @@ export function generatePreviewHTML(macaronFile: string, html: string): string {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Macaron</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Manrope:wght@200..800&family=Lexend:wght@200..800&family=Inconsolata:wght@500&display=swap">
       <style>
         body {
           margin: 0;
+          -webkit-font-smoothing: antialiased;
         }
       </style>
     </head>
