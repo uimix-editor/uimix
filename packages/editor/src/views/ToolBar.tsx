177a65851134986f3edc2dbe807f90391974365e
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-16 18:41:36 +0900

    feat(editor): add anonymous feedback button

--- packages/editor/src/views/ToolBar.tsx
+++ packages/editor/src/views/ToolBar.tsx
@@ -7,12 +7,31 @@ import { ToolBar as ToolBarWrap } from "@seanchas116/paintkit/src/components/too
 import {
   ToolButton,
   ToolButtonArray,
+  ToolButtonArrayGroup,
 } from "@seanchas116/paintkit/src/components/toolbar/ToolButton";
 import { Dropdown } from "@seanchas116/paintkit/src/components/menu/Dropdown";
 import { ZoomControl } from "@seanchas116/paintkit/src/components/toolbar/ZoomControl";
 import { observer } from "mobx-react-lite";
 import { action } from "mobx";
+import styled from "styled-components";
+import { colors } from "@seanchas116/paintkit/src/components/Palette";
 import { useEditorState } from "./useEditorState";
+
+const FeedbackButton = styled.a`
+  background-color: ${colors.active};
+  font-size: 12px;
+  line-height: 12px;
+  height: 20px;
+  color: ${colors.activeText};
+  padding: 4px 8px;
+  text-decoration: none;
+  border-radius: 10px;
+
+  :hover {
+    color: ${colors.activeText};
+    filter: saturate(1.5);
+  }
+`;
 
 export const ToolBar: React.FC = observer(() => {
   const editorState = useEditorState();
@@ -53,17 +72,26 @@ export const ToolBar: React.FC = observer(() => {
 
   return (
     <ToolBarWrap>
-      <Dropdown
-        options={editorState.getMainMenu()}
-        button={(open, onClick) => (
-          <ToolButton
-            label="Menu"
-            icon={menuIcon}
-            selected={open}
-            onClick={(_, elem) => onClick(elem)}
-          />
-        )}
-      />
+      <ToolButtonArrayGroup>
+        <Dropdown
+          options={editorState.getMainMenu()}
+          button={(open, onClick) => (
+            <ToolButton
+              label="Menu"
+              icon={menuIcon}
+              selected={open}
+              onClick={(_, elem) => onClick(elem)}
+            />
+          )}
+        />
+        <FeedbackButton
+          href="https://form.jotform.com/220732261468454"
+          target="_blank"
+        >
+          Feedback
+        </FeedbackButton>
+      </ToolButtonArrayGroup>
+
       <ToolButtonArray>
         <ToolButton
           selected={editorState.insertMode === "frame"}
