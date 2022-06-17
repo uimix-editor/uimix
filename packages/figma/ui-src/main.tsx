540d2edbda5bf8675e5ff7fc81d77eb3f2e124a4
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-17 15:07:45 +0900

    Handle event

--- packages/figma/ui-src/main.tsx
+++ packages/figma/ui-src/main.tsx
@@ -1,5 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import "./ui.macaron";
+import { MessageToPlugin, MessageToUI } from "../message";
+import { Buffer } from "buffer";
+
+let htmlToCopy: string | undefined;
+
+document.addEventListener("copy", (e) => {
+  if (htmlToCopy) {
+    e.preventDefault();
+    e.clipboardData?.setData("text/html", htmlToCopy);
+    htmlToCopy = undefined;
+  }
+});
+
+window.addEventListener("message", (e) => {
+  const msg: MessageToUI = e.data.pluginMessage;
+  if (msg.type === "copy") {
+    const fragmentString: string = msg.data;
+    const base64 = Buffer.from(fragmentString).toString("base64");
+    const encoded = `<span data-macaron="${base64}"></span>`;
+    htmlToCopy = encoded;
+    document.execCommand("copy");
+
+    postMessageToPlugin({
+      type: "notify",
+      data: "Copied to clipboard. Paste in Macaron",
+    });
+  }
+});
+
+function postMessageToPlugin(data: MessageToPlugin): void {
+  parent.postMessage({ pluginMessage: data }, "*");
+}
+
+const onCopy = () => {
+  postMessageToPlugin({ type: "copy" });
+};
+
+const ui = document.querySelector("macaron-figma-ui")!;
+
+const copyButton = ui.shadowRoot!.querySelector("#copy-button");
+
+copyButton?.addEventListener("click", onCopy);
