e2ba4ab6deb10e5028a1c97477cc0885e27e11a9
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-22 19:01:38 +0900

    Add loader-webpack

---
+++ packages/loader-webpack/index.js
@@ -0,0 +1,21 @@
+const { compile } = require("@macaron-elements/compiler");
+
+const fileRegex = /\.macaron$/;
+
+function macaronPlugin() {
+  return {
+    name: "macaron-loader",
+
+    transform(src, id) {
+      if (fileRegex.test(id)) {
+        return {
+          code: compile(src),
+          map: null, // provide source map if available
+        };
+      }
+    },
+  };
+}
+
+module.exports = macaronPlugin;
+module.exports.default = macaronPlugin;
