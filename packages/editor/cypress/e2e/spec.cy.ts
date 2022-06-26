e388af42a1511491bdfbb89525c5e72be75709ea
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-26 18:01:00 +0900

    Simple DOM-using testing

--- packages/editor/cypress/e2e/spec.cy.ts
+++ packages/editor/cypress/e2e/spec.cy.ts
@@ -1,7 +1,14 @@
 describe("empty spec", () => {
   it("passes", () => {
-    console.log(window);
+    const elem = document.createElement("div");
+    elem.style.width = "100px";
+    elem.style.height = "100px";
+    elem.style.backgroundColor = "red";
+    elem.style.position = "absolute";
+    elem.style.left = "0px";
+    elem.style.top = "0px";
+    document.body.appendChild(elem);
 
-    cy.visit("https://example.cypress.io");
+    expect(elem.clientWidth).to.equal(100);
   });
 });
