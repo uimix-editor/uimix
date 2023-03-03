5af9b937256697c951e5d83b370e44baead8bdf9
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-03 21:11:34 +0900

    Use esbuild watch mode + node-dev

--- packages/devserver/build.cjs
+++ packages/devserver/build.cjs
@@ -3,11 +3,18 @@ const esbuild = require("esbuild");
 
 const externals = Object.keys(require("./package.json").dependencies);
 
-esbuild.build({
+/** @type import('esbuild').BuildOptions */
+const options = {
   entryPoints: ["./src/cli.ts"],
   bundle: true,
   format: "esm",
   platform: "node",
   outfile: "dist/cli.js",
   external: externals,
-});
+};
+
+if (process.argv.includes("--watch")) {
+  esbuild.context(options).then((ctx) => ctx.watch());
+} else {
+  esbuild.build(options);
+}
