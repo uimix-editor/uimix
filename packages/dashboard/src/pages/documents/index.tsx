a94f6c29c7331dd1932b0446f0c529a0cb1b7034
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-06 00:30:06 +0900

    Invoke queries

--- packages/dashboard/src/pages/documents/index.tsx
+++ packages/dashboard/src/pages/documents/index.tsx
@@ -1,10 +1,31 @@
+import { trpc } from "../../utils/trpc";
+
 export default function Document() {
+  const documents = trpc.document.all.useQuery();
+  const documentCreateMutation = trpc.document.create.useMutation();
+
+  const onAddClick = () => {
+    documentCreateMutation.mutate({
+      title: "New document",
+    });
+  };
+
   return (
     <>
       <h1>Documents</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={onAddClick}
+      >
         Add
       </button>
+      <ul>
+        {documents.data?.map((document) => (
+          <li key={document.id}>
+            <a href={`/documents/${document.id}`}>{document.title}</a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
