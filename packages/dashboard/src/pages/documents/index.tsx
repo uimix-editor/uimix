ab75d6bf2405bbd5f507540ab3c05c356018e7ad
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-05 23:23:29 +0900

    WIP /documents/ page

---
+++ packages/dashboard/src/pages/documents/index.tsx
@@ -0,0 +1,10 @@
+export default function Document() {
+  return (
+    <>
+      <h1>Documents</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Add
+      </button>
+    </>
+  );
+}
