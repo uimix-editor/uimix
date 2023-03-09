d243d0aa043ed7414fc230b4acad54cbb2ec9d7d
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-09 14:19:22 +0900

    Add title

--- packages/dashboard/src/pages/documents/[id].tsx
+++ packages/dashboard/src/pages/documents/[id].tsx
@@ -14,6 +14,7 @@ const Document = () => {
   return (
     <>
       <Head>
+        <title>Document</title>
         <style>{`
           body {
             overscroll-behavior: none;
--- packages/dashboard/src/pages/documents/index.tsx
+++ packages/dashboard/src/pages/documents/index.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@iconify/react";
+import Head from "next/head";
 import Link from "next/link";
 import { trpc } from "../../utils/trpc";
 
@@ -14,38 +15,43 @@ export default function Documents() {
   };
 
   return (
-    <div className="text-xs">
-      <div className="h-10 border-b border-neutral-200 relative flex items-center justify-end px-4"></div>
-      <main className="max-w-[960px] mx-auto px-4">
-        <div className="flex justify-between items-center">
-          <h1 className="font-bold text-lg py-8">Documents</h1>
-          <button
-            className="h-fit bg-blue-500 hover:bg-blue-700 text-base text-white py-1 px-3 rounded flex items-center gap-1"
-            onClick={onAddClick}
-          >
-            <Icon icon="material-symbols:add" />
-            Add
-          </button>
-        </div>
-        <ul className="grid grid-cols-3 gap-4">
-          {documents.data?.map((document) => (
-            <li key={document.id}>
-              <Link
-                href={`/documents/${document.id}`}
-                className="block border border-gray-200 rounded-lg"
-              >
-                <div className="aspect-video w-full bg-gray-100" />
-                <div className="p-4">
-                  <div className="text-sm text-gray-900 font-medium mb-1">
-                    {document.title}
+    <>
+      <Head>
+        <title>Documents</title>
+      </Head>
+      <div className="text-xs">
+        <div className="h-10 border-b border-neutral-200 relative flex items-center justify-end px-4"></div>
+        <main className="max-w-[960px] mx-auto px-4">
+          <div className="flex justify-between items-center">
+            <h1 className="font-bold text-lg py-8">Documents</h1>
+            <button
+              className="h-fit bg-blue-500 hover:bg-blue-700 text-base text-white py-1 px-3 rounded flex items-center gap-1"
+              onClick={onAddClick}
+            >
+              <Icon icon="material-symbols:add" />
+              Add
+            </button>
+          </div>
+          <ul className="grid grid-cols-3 gap-4">
+            {documents.data?.map((document) => (
+              <li key={document.id}>
+                <Link
+                  href={`/documents/${document.id}`}
+                  className="block border border-gray-200 rounded-lg"
+                >
+                  <div className="aspect-video w-full bg-gray-100" />
+                  <div className="p-4">
+                    <div className="text-sm text-gray-900 font-medium mb-1">
+                      {document.title}
+                    </div>
+                    <div className="text-gray-500">Edited 3 days ago</div>
                   </div>
-                  <div className="text-gray-500">Edited 3 days ago</div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
+    </>
   );
 }
