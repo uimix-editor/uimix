5c0442f504c3b81c0cd81f061dbaae5ba6deb99e
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-12 16:20:27 +0900

    Remove unused code

--- packages/dashboard/src/pages/api/auth/[...nextauth].ts
+++ packages/dashboard/src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import FigmaProvider from "../../../lib/auth/figma";
 import { db } from "../../../lib/prismadb";
 
 export const authOptions: AuthOptions = {
--- packages/dashboard/src/server/routers/_app.ts
+++ packages/dashboard/src/server/routers/_app.ts
@@ -1,13 +1,9 @@
 import { router } from "../trpc";
 import { collaborativeRouter } from "./collaborative";
 import { documentRouter } from "./document";
-import { figmaRouter } from "./figma";
-import { githubRouter } from "./github";
 import { imageRouter } from "./image";
 
 export const appRouter = router({
-  figma: figmaRouter,
-  github: githubRouter,
   document: documentRouter,
   collaborative: collaborativeRouter,
   image: imageRouter,
