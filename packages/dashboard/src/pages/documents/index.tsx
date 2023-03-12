02e83a98b6227b7c7e32d9824266d8f75152b531
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-13 01:20:10 +0900

    Fix

--- packages/dashboard/src/pages/documents/index.tsx
+++ packages/dashboard/src/pages/documents/index.tsx
@@ -121,7 +121,7 @@ export default function Documents() {
                 <li key={document.id}>
                   <Link
                     href={`/documents/${document.id}`}
-                    className="block border border-gray-200 rounded-lg hover:bg-gray-50"
+                    className="block border border-gray-200 rounded-lg hover:bg-gray-50 overflow-hidden"
                   >
                     <div className="aspect-video w-full bg-gray-100" />
                     <div className="p-4 flex justify-between items-center">
