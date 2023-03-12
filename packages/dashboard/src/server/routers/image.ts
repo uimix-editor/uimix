0789a59b74bc2d559c78228211b09fb3d8bb9b0b
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-12 16:27:07 +0900

    Cleanup

--- packages/dashboard/src/server/routers/image.ts
+++ packages/dashboard/src/server/routers/image.ts
@@ -18,15 +18,6 @@ export const imageRouter = router({
     )
     .query(async ({ ctx, input }) => {
       const currentUser = await authenticate(ctx.req);
-      // await s3
-      //   .putObject({
-      //     Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-      //     Key: `images/${currentUser.id}/${input.hash}`,
-      //     ContentType: input.contentType,
-      //     Body: input.hash,
-      //     ACL: "public-read",
-      //   })
-      //   .promise();
 
       const uploadURL = await s3.getSignedUrl("putObject", {
         Bucket: process.env.AWS_S3_BUCKET_NAME,
