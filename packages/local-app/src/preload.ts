818ceb19e371be6cdad6c5166a683f66a6f7b58d
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-15 17:54:54 +0900

    Expose API

--- packages/local-app/src/preload.ts
+++ packages/local-app/src/preload.ts
@@ -1,2 +1,10 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+// @ts-ignore
+
+import { contextBridge } from "electron";
+
+contextBridge.exposeInMainWorld("myAPI", {
+  desktop: true,
+});
