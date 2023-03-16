2c421dd3acd1edf01cb4c9d4dd264f5cc5cd6306
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-16 20:14:33 +0900

    Fix test

--- packages/cli/src/compiler/generateCode.test.ts
+++ packages/cli/src/compiler/generateCode.test.ts
@@ -4,12 +4,20 @@ import * as fs from "fs";
 import { generateCode } from "./generateCode";
 
 describe(generateCode.name, () => {
-  it("generates code", () => {
+  it("generates code", async () => {
     const uimixPath = "../../../sandbox/src/uimix";
     const json = JSON.parse(
-      fs.readFileSync(path.resolve(__dirname, uimixPath, "data.json"), "utf8")
+      fs.readFileSync(
+        path.resolve(__dirname, uimixPath, "components.uimix"),
+        "utf8"
+      )
     );
-    const code = generateCode(".", "components", json);
+    const code = await generateCode("../..", "components", json);
+    for (const file of code) {
+      if (file.content instanceof Buffer) {
+        file.content = "some binary data";
+      }
+    }
     expect(code).toMatchSnapshot();
   });
 });
