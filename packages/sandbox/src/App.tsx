271b1f242242adeba9d96edfd7a7036c7a6f1f3e
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-16 20:15:01 +0900

    Fix

--- packages/sandbox/src/App.tsx
+++ packages/sandbox/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button, Page } from "./uimix";
+import { Button, Page } from "./uimix/components";
 
 function App() {
   const [count, setCount] = useState(0);
