cb0e15aa5620feb847e6e9e716ff4980353b8002
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-18 14:08:58 +0900

    Tweak default window size of Electron app

--- packages/local-app/src/index.ts
+++ packages/local-app/src/index.ts
@@ -11,8 +11,8 @@ if (require("electron-squirrel-startup")) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
