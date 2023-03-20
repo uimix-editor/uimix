1999aa56ca15dbfb5b46fea2556a6c296775fbb2
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-20 17:30:03 +0900

    Fix

--- packages/local-app/forge.config.js
+++ packages/local-app/forge.config.js
@@ -1,22 +1,42 @@
+const { execSync } = require("child_process");
+
 module.exports = {
+  hooks: {
+    prePackage: async () => {
+      execSync("npm run build", { stdio: "inherit" });
+    },
+  },
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        authToken: require("./forge.secrets.json").githubToken,
+        repository: {
+          owner: "uimix-editor",
+          name: "uimix",
+        },
+        prerelease: true,
+      },
     },
   ],
 };
