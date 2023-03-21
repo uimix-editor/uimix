6cf6dec04a1ea464de0ece0e7fb9fe78a6488b93
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-21 11:40:59 +0900

    Fix lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./packages/*/tsconfig.json"],
+    project: ["./packages/*/tsconfig.json", "./packages/*/*/tsconfig.json"],
   },
   plugins: ["@typescript-eslint"],
   extends: [
