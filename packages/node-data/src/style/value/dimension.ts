63afe1b46577d4cae79ad74765130965b4d975dd
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-22 18:25:39 +0900

    Revert "Setup migration"
    This reverts commit ccf9444ed733e272b9efa7d29fafb44c1b3a7104.

--- packages/node-data/src/style/value/dimension.ts
+++ packages/node-data/src/style/value/dimension.ts
@@ -1,15 +1,7 @@
 import { z } from "zod";
 
-export const PxValue = z
-  .union([z.number(), z.tuple([z.number(), z.literal("px")])])
-  .transform((value): [number, "px"] =>
-    typeof value === "number" ? [value, "px"] : value
-  );
-export const PxPercentValue = z
-  .union([
-    z.number(),
-    z.tuple([z.number(), z.union([z.literal("px"), z.literal("%")])]),
-  ])
-  .transform((value): [number, "px" | "%"] =>
-    typeof value === "number" ? [value, "px"] : value
-  );
+export const PxValue = z.tuple([z.number(), z.literal("px")]);
+export const PxPercentValue = z.tuple([
+  z.number(),
+  z.union([z.literal("px"), z.literal("%")]),
+]);
