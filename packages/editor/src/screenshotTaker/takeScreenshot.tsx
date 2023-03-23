f9e7d3bcfb19d1ec0dfc059de464f0fa739a29a7
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-23 12:44:50 +0900

    Render thumbnail

--- packages/editor/src/screenshotTaker/takeScreenshot.tsx
+++ packages/editor/src/screenshotTaker/takeScreenshot.tsx
@@ -1,20 +1,50 @@
 import { Project } from "../models/Project";
 import { domForSelectable } from "../views/viewport/renderer/NodeRenderer";
 import * as htmlToImage from "html-to-image";
+import { Rect, Vec2 } from "paintvec";
 
-export function takeScreenshot(project: Project) {
+export async function takeScreenshot(project: Project) {
   const firstPage = project.pages.all[0];
   if (!firstPage) {
     return;
   }
+
+  const thumbSize = new Vec2(640, 360);
+  const canvas = document.createElement("canvas");
+  canvas.width = thumbSize.x;
+  canvas.height = thumbSize.y;
+  const ctx = canvas.getContext("2d")!;
+
+  const contentBBox = Rect.union(
+    ...firstPage.selectable.children.map((child) => child.computedRect)
+  );
+  if (!contentBBox) {
+    return;
+  }
+
+  const scale = Math.min(
+    thumbSize.x / contentBBox.width,
+    thumbSize.y / contentBBox.height
+  );
 
   for (const selectable of firstPage.selectable.children) {
     const dom = domForSelectable.get(selectable);
     if (!dom) {
       return;
     }
-    void htmlToImage.toSvg(dom).then((svg) => {
-      console.log(svg);
+
+    const domCanvas = await htmlToImage.toCanvas(dom, {
+      skipFonts: true,
     });
+
+    ctx.drawImage(
+      domCanvas,
+      (selectable.computedRect.left - contentBBox.left) * scale,
+      (selectable.computedRect.left - contentBBox.left) * scale,
+      selectable.computedRect.width * scale,
+      selectable.computedRect.height * scale
+    );
   }
+
+  return canvas.toDataURL();
 }
