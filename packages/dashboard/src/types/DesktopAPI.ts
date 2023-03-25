e9bfe2e92bed3396b66ee02c533317c636b2a37d
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-25 17:53:31 +0900

    Simplify desktop api

--- packages/dashboard/src/types/DesktopAPI.ts
+++ packages/dashboard/src/types/DesktopAPI.ts
@@ -12,17 +12,20 @@ export interface LocalDocument {
 }
 
 export interface DesktopAPI {
-  desktop: true;
-  wait(ms: number): Promise<void>;
-  getLocalDocuments(): Promise<readonly LocalDocument[]>;
-  getLocalDocument(id: string): Promise<LocalDocument | undefined>;
-  createLocalDocument(): Promise<LocalDocument | undefined>;
-  addExistingLocalDocument(): Promise<LocalDocument | undefined>;
-  deleteLocalDocument(id: string): Promise<void>;
-  updateLocalDocumentThumbnail(id: string, pngData: Uint8Array): Promise<void>;
+  getDocumentData(): Promise<ProjectJSON>;
+  setDocumentData(data: ProjectJSON): Promise<void>;
+  onDocumentDataChange(callback: (data: ProjectJSON) => void): () => void;
 
-  getLocalDocumentData(id: string): Promise<ProjectJSON>;
-  setLocalDocumentData(id: string, data: ProjectJSON): Promise<void>;
+  // wait(ms: number): Promise<void>;
+  // getLocalDocuments(): Promise<readonly LocalDocument[]>;
+  // getLocalDocument(id: string): Promise<LocalDocument | undefined>;
+  // createLocalDocument(): Promise<LocalDocument | undefined>;
+  // addExistingLocalDocument(): Promise<LocalDocument | undefined>;
+  // deleteLocalDocument(id: string): Promise<void>;
+  // updateLocalDocumentThumbnail(id: string, pngData: Uint8Array): Promise<void>;
+
+  // getLocalDocumentData(id: string): Promise<ProjectJSON>;
+  // setLocalDocumentData(id: string, data: ProjectJSON): Promise<void>;
 }
 
 export function getDesktopAPI(): DesktopAPI | undefined {
