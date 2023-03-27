77fd5d45357e075d4f70ddbc9001b53315a2c27d
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-27 13:35:39 +0900

    Change clipboard data

--- packages/node-data/src/clipboard.ts
+++ packages/node-data/src/clipboard.ts
@@ -1,8 +1,30 @@
 import { z } from "zod";
-import { ProjectJSON } from "./project";
+import { NodeJSON } from "./node/node";
+import { Image, ProjectJSON } from "./project";
+import { StyleJSON } from "./style/style";
+
+const NodeHierarchyBase = NodeJSON.omit({
+  index: true,
+  parent: true,
+}).extend({
+  id: z.string(),
+  style: StyleJSON.partial(),
+});
+
+export type NodeHierarchy = z.infer<typeof NodeHierarchyBase> & {
+  children: NodeHierarchy[];
+};
+
+export const NodeHierarchy: z.ZodType<NodeHierarchy> = NodeHierarchyBase.extend(
+  {
+    children: z.lazy(() => z.array(NodeHierarchy)),
+  }
+);
 
 export const JSONClipboardData = z.object({
-  uimixNodes: ProjectJSON,
+  uimixClipboardVersion: z.literal("0.0.1"),
+  nodes: z.array(NodeHierarchy),
+  images: z.record(Image), // URLs must be data URLs
 });
 
 export type JSONClipboardData = z.infer<typeof JSONClipboardData>;
