bdb7a80ab9ea9db6d2a3ce2b1d51b71dd2e7d1b1
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-27 12:22:11 +0900

    Better component creation

--- packages/editor/src/services/CreateComponent.ts
+++ packages/editor/src/services/CreateComponent.ts
@@ -10,12 +10,28 @@ export function createComponent(selectable: Selectable) {
   if (selectable.idPath.length > 1) {
     return;
   }
-  if (selectable.ancestors.some((a) => a.node.type === "component")) {
+  if (selectable.originalNode.type !== "frame") {
+    // TODO: allow non-frame components?
     return;
   }
+
+  const parent = selectable.parent;
+  const next = selectable.nextSibling;
 
   const component = projectState.project.nodes.create("component");
   component.name = selectable.node.name;
   component.append([selectable.node]);
   page.append([component]);
+
+  selectable.style.position = {
+    // TODO: find better position
+    x: { type: "start", start: 0 },
+    y: { type: "start", start: 0 },
+  };
+
+  const instance = projectState.project.nodes.create("instance");
+  instance.name = selectable.node.name;
+  instance.selectable.style.mainComponent = component.id;
+
+  parent?.insertBefore(next, [instance.selectable]);
 }
