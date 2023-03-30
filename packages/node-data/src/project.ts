6d8217240c48af9e9eacbbbc2455b78ade7d8ada
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-30 18:30:35 +0900

    Fix

--- packages/node-data/src/project.ts
+++ packages/node-data/src/project.ts
@@ -2,11 +2,14 @@ import { z } from "zod";
 import { NodeJSON } from "./node/node.js";
 import { StyleJSON } from "./style/style.js";
 
+export const ImageType = z.enum(["image/png", "image/jpeg"]);
+export type ImageType = z.infer<typeof ImageType>;
+
 export const Image = z.object({
   width: z.number(),
   height: z.number(),
+  type: ImageType,
   url: z.string(),
-  type: z.enum(["image/png", "image/jpeg"]),
 });
 
 export type Image = z.infer<typeof Image>;
