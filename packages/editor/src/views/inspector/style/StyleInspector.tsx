8d26e833557e40685a25c497067a1024d65af6cf
Ryohei Ikegami <iofg2100@gmail.com>
2023-03-30 20:56:29 +0900

    Banner when editing group style

--- packages/editor/src/views/inspector/style/StyleInspector.tsx
+++ packages/editor/src/views/inspector/style/StyleInspector.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@iconify/react";
 import { BorderPane } from "./BorderPane";
 import { DebugPane } from "./DebugPane";
 import { DimensionsPane } from "./DimensionsPane";
@@ -11,6 +12,30 @@ import { TextPane } from "./TextPane";
 export function StyleInspector(): JSX.Element {
   return (
     <div className="flex flex-col">
+      <div className="flex p-3 border-b border-macaron-uiBackground">
+        <div className="bg-macaron-active rounded-full px-2 py-1 text-xs text-macaron-activeText font-medium">
+          Hover
+        </div>
+        <div className="rounded-full px-2 py-1 text-xs text-macaron-label font-medium">
+          Active
+        </div>
+        <div className="rounded-full px-2 py-1 text-xs text-macaron-label font-medium">
+          Focus
+        </div>
+      </div>
+      <div
+        className="
+      bg-purple-500 text-white p-3 font-medium flex items-center
+      "
+      >
+        Button_1 › Hover
+        <button className="border bg-white p-1 rounded-full ml-auto">
+          <Icon
+            icon="material-symbols:close"
+            className="font-xs text-purple-500"
+          />
+        </button>
+      </div>
       <DimensionsPane />
       <LayoutPane />
       <FillPane />
