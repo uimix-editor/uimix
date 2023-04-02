462565680128b52f9a19165633c740d637c6542d
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-02 23:58:40 +0900

    Implement

--- packages/editor/src/models/ColorToken.ts
+++ packages/editor/src/models/ColorToken.ts
@@ -3,6 +3,7 @@ import * as Y from "yjs";
 import { ObservableYMap } from "../utils/ObservableYMap";
 import { Project } from "./Project";
 import { getOrCreate } from "../state/Collection";
+import { generateID } from "../utils/ID";
 
 export class ColorToken {
   constructor(project: Project, id: string) {
@@ -71,9 +72,25 @@ export class ColorTokenList {
     return ObservableYMap.get(this.project.doc.getMap("colors"));
   }
 
+  get(id: string): ColorToken | undefined {
+    if (!this.data.has(id)) {
+      return undefined;
+    }
+    return new ColorToken(this.project, id);
+  }
+
   get all(): ColorToken[] {
     return [...this.data.keys()]
       .map((id) => new ColorToken(this.project, id))
       .sort((a, b) => a.index - b.index);
+  }
+
+  add(): ColorToken {
+    const id = generateID();
+
+    const token = new ColorToken(this.project, id);
+    token.index = this.all.length;
+    token.name = "New Color";
+    return token;
   }
 }
--- packages/editor/src/views/inspector/document/ColorTokenListPane.tsx
+++ packages/editor/src/views/inspector/document/ColorTokenListPane.tsx
@@ -1,15 +1,75 @@
 import { observer } from "mobx-react-lite";
 import { InspectorHeading } from "../components/InspectorHeading";
 import { InspectorPane } from "../components/InspectorPane";
+import { IconButton } from "../../../components/IconButton";
+import { ReactSortable } from "react-sortablejs";
+import { projectState } from "../../../state/ProjectState";
+import { action } from "mobx";
+import { compact } from "lodash-es";
+import { DoubleClickToEdit } from "../../../components/DoubleClickToEdit";
+import { ColorPopoverButton } from "../components/ColorInput";
+import { Color } from "../../../utils/Color";
 
 export const ColorTokenListPane = observer(() => {
+  const tokens = projectState.project.colorTokens.all;
+
+  // TODO: use list view?
+
   return (
     <InspectorPane>
       <InspectorHeading
         icon="material-symbols:palette-outline"
         text="Color Tokens"
+        buttons={
+          <>
+            <IconButton
+              icon="material-symbols:add"
+              onClick={action(() => {
+                projectState.project.colorTokens.add();
+                projectState.undoManager.stopCapturing();
+              })}
+            />
+          </>
+        }
       />
-      <div>TODO</div>
+      <div>
+        <ReactSortable
+          list={tokens.map((token) => ({
+            id: token.id,
+          }))}
+          setList={action((list: { id: string }[]) => {
+            const newTokens = compact(
+              list.map((item) => projectState.project.colorTokens.get(item.id))
+            );
+            for (const [i, token] of newTokens.entries()) {
+              token.index = i;
+            }
+            projectState.undoManager.stopCapturing();
+          })}
+        >
+          {tokens.map((token) => (
+            <div className="h-7 gap-2 flex" key={token.id}>
+              <ColorPopoverButton
+                value={token.value ?? Color.black}
+                onChange={action((color: Color) => {
+                  token.value = color;
+                })}
+                onChangeEnd={action(() => {
+                  projectState.undoManager.stopCapturing();
+                })}
+              />
+              <DoubleClickToEdit
+                className="flex-1"
+                value={token.name ?? ""}
+                onChange={action((name: string) => {
+                  token.name = name;
+                  projectState.undoManager.stopCapturing();
+                })}
+              />
+            </div>
+          ))}
+        </ReactSortable>
+      </div>
     </InspectorPane>
   );
 });
