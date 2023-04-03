aae41bbe86ab3704de58e4ca0ecfb34acaf14c02
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-03 23:56:09 +0900

    Cleanup

--- packages/editor/src/views/viewport/FloatingZoomControl.tsx
+++ packages/editor/src/views/viewport/FloatingZoomControl.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { action } from "mobx";
 import addIcon from "@iconify-icons/ic/add";
 import removeIcon from "@iconify-icons/ic/remove";
