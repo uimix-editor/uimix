b598d3154401965c70a3ae5fb58f70a87f2b2738
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-04 12:40:55 +0900

    Upgrade storybook to v7 (editor)

--- packages/editor/.storybook/main.cjs
+++ packages/editor/.storybook/main.cjs
@@ -1,18 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
