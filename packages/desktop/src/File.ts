64d68a882d39ffb0779e42427f50b6182fee0ee2
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-05 22:48:01 +0900

    Move project file load/save to cli

--- packages/desktop/src/File.ts
+++ packages/desktop/src/File.ts
@@ -1,22 +1,16 @@
-import fs from "fs";
 import path from "path";
-import prettier from "prettier/standalone";
-import parserBabel from "prettier/parser-babel";
-import {
-  NodeJSON,
-  PageJSON,
-  ProjectJSON,
-  ProjectManifestJSON,
-  StyleJSON,
-} from "../../node-data/src";
+import { ProjectJSON } from "../../node-data/src";
 import { DocumentMetadata } from "../../dashboard/src/types/DesktopAPI";
 import { TypedEmitter } from "tiny-typed-emitter";
 import { app, dialog } from "electron";
-import { isEqual, omit } from "lodash";
+import { isEqual } from "lodash";
 import chokidar from "chokidar";
-import { sha256 } from "js-sha256";
-import { mkdirpSync } from "mkdirp";
-import { glob } from "glob";
+import {
+  loadProjectFromDirectory,
+  saveProjectToDirectory,
+  filesToProjectJSON,
+  projectJSONToFiles,
+} from "../../cli/src/compiler/project";
 
 export function compareProjectJSONs(a: ProjectJSON, b: ProjectJSON): boolean {
   return (
@@ -25,220 +19,6 @@ export function compareProjectJSONs(a: ProjectJSON, b: ProjectJSON): boolean {
     JSON.stringify(a.componentURLs) === JSON.stringify(b.componentURLs)
     // do not compare images because they are too big
   );
-}
-
-interface HierarchicalNodeJSON extends NodeJSON {
-  id: string;
-  children: HierarchicalNodeJSON[];
-}
-
-function toHierarchicalNodeJSONs(
-  nodes: Record<string, NodeJSON>
-): HierarchicalNodeJSON[] {
-  const hierarchicalNodes: Record<string, HierarchicalNodeJSON> = {};
-
-  for (const [id, node] of Object.entries(nodes)) {
-    hierarchicalNodes[id] = {
-      ...node,
-      id,
-      children: [],
-    };
-  }
-
-  for (const [id, node] of Object.entries(nodes)) {
-    if (node.parent) {
-      hierarchicalNodes[node.parent].children.push(hierarchicalNodes[id]);
-    }
-  }
-
-  for (const node of Object.values(hierarchicalNodes)) {
-    node.children.sort((a, b) => a.index - b.index);
-  }
-
-  return Object.values(hierarchicalNodes).filter((node) => !node.parent);
-}
-
-interface ProjectJSONFiles {
-  manifest: ProjectManifestJSON;
-  pages: Map<string /* file path */, PageJSON>;
-}
-
-function projectJSONToFiles(projectJSON: ProjectJSON): ProjectJSONFiles {
-  const manifest: ProjectManifestJSON = {
-    componentURLs: projectJSON.componentURLs,
-    images: projectJSON.images,
-    colors: projectJSON.colors,
-  };
-
-  const hierarchicalNodes = toHierarchicalNodeJSONs(projectJSON.nodes);
-  const projectNode = hierarchicalNodes.find((node) => node.type === "project");
-  if (!projectNode) {
-    throw new Error("Project node not found");
-  }
-
-  const pages = new Map<string, PageJSON>();
-  const nodeIDToPageJSON = new Map<string, PageJSON>();
-  for (const page of projectNode.children) {
-    if (page.type !== "page") {
-      throw new Error("expected page node");
-    }
-    if (!page.name) {
-      throw new Error("page name is empty");
-    }
-
-    const pageJSON: PageJSON = {
-      nodes: {},
-      styles: {},
-    };
-    const addNodeRecursively = (node: HierarchicalNodeJSON) => {
-      pageJSON.nodes[node.id] = omit(node, ["children", "id"]);
-      nodeIDToPageJSON.set(node.id, pageJSON);
-      for (const child of node.children) {
-        addNodeRecursively(child);
-      }
-    };
-    for (const child of page.children) {
-      addNodeRecursively(omit(child, ["parent"]));
-    }
-    pages.set(page.name, pageJSON);
-  }
-
-  for (const [id, style] of Object.entries(projectJSON.styles)) {
-    const idPath = id.split(":");
-    const pageJSON = nodeIDToPageJSON.get(idPath[0]);
-    if (pageJSON) {
-      pageJSON.styles[id] = style;
-    }
-  }
-
-  return {
-    manifest,
-    pages,
-  };
-}
-
-function saveProjectToDirectory(
-  projectDirPath: string,
-  files: ProjectJSONFiles
-): void {
-  mkdirpSync(path.resolve(projectDirPath, "uimix"));
-  mkdirpSync(path.resolve(projectDirPath, "uimix/images"));
-
-  fs.writeFileSync(
-    path.resolve(projectDirPath, "uimix/project.json"),
-    formatJSON(JSON.stringify(files.manifest))
-  );
-
-  for (const [hash, image] of Object.entries(files.manifest.images ?? {})) {
-    const dataURL = image.url;
-    // data url to buffer
-    const base64 = dataURL.split(",")[1];
-    const buffer = Buffer.from(base64, "base64");
-
-    fs.writeFileSync(
-      path.resolve(projectDirPath, `uimix/images/${hash}.png`),
-      buffer
-    );
-  }
-
-  const pagePathsToDelete = new Set(
-    glob.sync("**/*.uimix", {
-      cwd: projectDirPath,
-    })
-  );
-
-  for (const [pageName, pageJSON] of files.pages) {
-    const pagePath = pageName + ".uimix";
-    const pageDirPath = path.dirname(pagePath);
-    mkdirpSync(path.resolve(projectDirPath, pageDirPath));
-    fs.writeFileSync(
-      path.resolve(projectDirPath, pagePath),
-      formatJSON(JSON.stringify(pageJSON))
-    );
-    pagePathsToDelete.delete(pagePath);
-  }
-
-  for (const pagePath of pagePathsToDelete) {
-    fs.rmSync(path.resolve(projectDirPath, pagePath));
-  }
-}
-
-function filesToProjectJSON(files: ProjectJSONFiles): ProjectJSON {
-  // TODO: detect ID conflicts between files (and provide way to resolve them)
-
-  const projectJSON: ProjectJSON = {
-    nodes: {},
-    styles: {},
-    componentURLs: files.manifest.componentURLs,
-    images: files.manifest.images,
-    colors: files.manifest.colors,
-  };
-
-  const projectNode: NodeJSON = {
-    type: "project",
-    index: 0,
-  };
-  projectJSON.nodes["project"] = projectNode;
-
-  let pageIndex = 0;
-  for (const [pageName, pageJSON] of files.pages) {
-    const pageID: string = sha256(pageName);
-
-    const pageNode: NodeJSON = {
-      type: "page",
-      index: pageIndex++,
-      name: pageName,
-      parent: "project",
-    };
-    projectJSON.nodes[pageID] = pageNode;
-
-    for (const [id, node] of Object.entries(pageJSON.nodes)) {
-      projectJSON.nodes[id] = {
-        ...node,
-        parent: node.parent ?? pageID,
-      };
-    }
-
-    for (const [id, style] of Object.entries(pageJSON.styles)) {
-      projectJSON.styles[id] = style;
-    }
-  }
-
-  return projectJSON;
-}
-
-function loadProjectFromDirectory(projectDirPath: string): ProjectJSONFiles {
-  const manifestPath = path.resolve(projectDirPath, "uimix/project.json");
-  let manifest: ProjectManifestJSON;
-  try {
-    manifest = ProjectManifestJSON.parse(
-      JSON.parse(fs.readFileSync(manifestPath, { encoding: "utf-8" }))
-    );
-  } catch {
-    manifest = { componentURLs: [], images: {}, colors: {} };
-  }
-
-  const pages = new Map<string, PageJSON>();
-
-  const pagePaths = glob.sync("**/*.uimix", {
-    cwd: projectDirPath,
-  });
-
-  for (const pagePath of pagePaths) {
-    const pageJSON = PageJSON.parse(
-      JSON.parse(
-        fs.readFileSync(path.resolve(projectDirPath, pagePath), {
-          encoding: "utf-8",
-        })
-      )
-    );
-    pages.set(pagePath.replace(/\.uimix$/, ""), pageJSON);
-  }
-
-  return {
-    manifest,
-    pages,
-  };
 }
 
 export class File extends TypedEmitter<{
@@ -382,11 +162,4 @@ export class File extends TypedEmitter<{
     };
   }
   private watchDisposer?: () => void;
-}
-
-function formatJSON(text: string): string {
-  return prettier.format(text, {
-    parser: "json",
-    plugins: [parserBabel],
-  });
 }
