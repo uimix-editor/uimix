07059e0a30dc515ffd75a806fab63db34959d39f
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-05 13:32:16 +0900

    Add new data types

--- packages/node-data/src/project.ts
+++ packages/node-data/src/project.ts
@@ -30,3 +30,16 @@ export const ProjectJSON = z.object({
 });
 
 export type ProjectJSON = z.infer<typeof ProjectJSON>;
+
+export const PageJSON = z.object({
+  nodes: z.record(NodeJSON).optional(),
+  styles: z.record(StyleJSON.partial()).optional(),
+});
+export type PageJSON = z.infer<typeof PageJSON>;
+
+export const ProjectManifestJSON = z.object({
+  componentURLs: z.array(z.string()).optional(),
+  images: z.record(Image).optional(), // TODO: store images in separate files
+  colors: z.record(ColorToken).optional(),
+});
+export type ProjectManifestJSON = z.infer<typeof ProjectManifestJSON>;
