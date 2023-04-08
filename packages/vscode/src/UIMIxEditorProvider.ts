5c1414a4ba525589da71289f3cdc05f0bd487776
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-08 10:18:02 +0900

    WIP import projectFiles

--- packages/vscode/src/UIMIxEditorProvider.ts
+++ packages/vscode/src/UIMIxEditorProvider.ts
@@ -1,12 +1,22 @@
 import * as vscode from "vscode";
 import { UIMixDocument } from "./UIMixDocument";
+import { ProjectFiles } from "uimix/src/compiler/ProjectFiles";
 
 export class UIMixEditorProvider implements vscode.CustomEditorProvider {
   constructor(context: vscode.ExtensionContext) {
     this.context = context;
+
+    const rootFolder = vscode.workspace.workspaceFolders?.[0];
+    if (!rootFolder) {
+      throw new Error("No workspace folder found");
+    }
+
+    this.projectFiles = new ProjectFiles(rootFolder.uri.fsPath);
+    console.log(this.projectFiles.pages);
   }
 
   readonly context: vscode.ExtensionContext;
+  readonly projectFiles: ProjectFiles;
 
   private readonly _onDidChangeCustomDocument =
     new vscode.EventEmitter<vscode.CustomDocumentContentChangeEvent>();
