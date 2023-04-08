c326d361413f0333eeeb6422f1384cb67c773bb4
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-08 23:37:28 +0900

    Simple test

--- packages/cli/src/compiler/ProjectFiles.test.ts
+++ packages/cli/src/compiler/ProjectFiles.test.ts
@@ -18,47 +18,43 @@ describe(ProjectFiles.name, () => {
     tmpObj = tmp.dirSync({
       unsafeCleanup: true,
     });
-
-    shell.cp(
-      "-R",
-      path.resolve(__dirname, "__fixtures__/demo-project"),
-      tmpObj.name
-    );
+    shell.mkdir("-p", tmpObj.name + "/demo-project");
   });
 
   afterEach(async () => {
     tmpObj.removeCallback();
   });
 
-  it("loads", () => {
-    const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
-    projectFiles.load();
+  // it("loads", () => {
+  //   const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
+  //   projectFiles.load();
 
-    expect(projectFiles.toProjectJSON()).toMatchSnapshot();
-  });
+  //   expect(projectFiles.toProjectJSON()).toMatchSnapshot();
+  // });
 
   it("saves", () => {
-    const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
-    projectFiles.load();
-
     const ydoc = new Y.Doc();
     const project = new Project(ydoc);
-    loadProjectJSON(ydoc, projectFiles.toProjectJSON());
-    console.log(project.node.id);
+    const page1 = project.pages.create("src/page1");
+    const page2 = project.pages.create("src/page2");
 
-    project.pages.all[0].node.children[0].name = "Hello World!";
+    page1.node.append([project.nodes.create("frame")]);
+    page2.node.append([project.nodes.create("text")]);
 
+    const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
     projectFiles.loadProjectJSON(toProjectJSON(ydoc));
-
     projectFiles.save();
 
-    // check if components.uimix is correct
-
-    const componentsUimix = fs.readFileSync(
-      tmpObj.name + "/demo-project/src/components.uimix",
-      "utf-8"
+    const page1File = fs.readFileSync(
+      tmpObj.name + "/demo-project/src/page1.uimix",
+      "utf8"
     );
+    expect(page1File).toMatchSnapshot();
 
-    expect(componentsUimix).toMatchSnapshot();
+    const page2File = fs.readFileSync(
+      tmpObj.name + "/demo-project/src/page2.uimix",
+      "utf8"
+    );
+    expect(page2File).toMatchSnapshot();
   });
 });
