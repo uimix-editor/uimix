ed53cd74e38427dc1e34bbc5d5b52dd6f42db9b5
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-08 22:59:42 +0900

    Test save

--- packages/cli/src/compiler/ProjectFiles.test.ts
+++ packages/cli/src/compiler/ProjectFiles.test.ts
@@ -4,6 +4,12 @@ import * as path from "path";
 import * as fs from "fs";
 import shell from "shelljs";
 import tmp from "tmp";
+import { Project } from "@uimix/editor/src/models/Project";
+import * as Y from "yjs";
+import {
+  loadProjectJSON,
+  toProjectJSON,
+} from "@uimix/editor/src/models/ProjectJSON";
 
 describe(ProjectFiles.name, () => {
   let tmpObj: tmp.DirResult;
@@ -24,10 +30,35 @@ describe(ProjectFiles.name, () => {
     tmpObj.removeCallback();
   });
 
-  it("should work", () => {
+  it("loads", () => {
     const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
     projectFiles.load();
 
     expect(projectFiles.toProjectJSON()).toMatchSnapshot();
+  });
+
+  it("saves", () => {
+    const projectFiles = new ProjectFiles(tmpObj.name + "/demo-project");
+    projectFiles.load();
+
+    const ydoc = new Y.Doc();
+    const project = new Project(ydoc);
+    loadProjectJSON(ydoc, projectFiles.toProjectJSON());
+    console.log(project.node.id);
+
+    project.pages.all[0].node.children[0].name = "Hello World!";
+
+    projectFiles.loadProjectJSON(toProjectJSON(ydoc));
+
+    projectFiles.save();
+
+    // check if components.uimix is correct
+
+    const componentsUimix = fs.readFileSync(
+      tmpObj.name + "/demo-project/src/components.uimix",
+      "utf-8"
+    );
+
+    expect(componentsUimix).toMatchSnapshot();
   });
 });
