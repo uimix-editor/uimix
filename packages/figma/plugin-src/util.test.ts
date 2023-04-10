822eaf0d54ecb3b428477187eee06d2c1449603e
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-10 15:41:32 +0900

    Update snapshot

--- packages/figma/plugin-src/util.test.ts
+++ packages/figma/plugin-src/util.test.ts
@@ -5,6 +5,6 @@ describe(getURLSafeBase64Hash.name, () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   it("works", () => {
     const result = getURLSafeBase64Hash(Buffer.from("hello world"));
-    expect(result).toMatchInlineSnapshot('"uU0nuZNNPgilLlLX2n2r-sSE7-N6U4DukIj3rOLvzek="');
+    expect(result).toMatchInlineSnapshot('"uU0nuZNNPgilLlLX2n2r-sSE7-N6U4DukIj3rOLvzek"');
   });
 });
