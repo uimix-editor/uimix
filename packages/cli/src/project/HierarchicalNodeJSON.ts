add1f9ac38a122484dd6b01de5d0cd83ee6e049c
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-13 15:02:00 +0900

    Add HierarchicalNodeJSON

---
+++ packages/cli/src/project/HierarchicalNodeJSON.ts
@@ -0,0 +1,32 @@
+import { NodeJSON } from "@uimix/model/src/data/v1";
+
+export interface HierarchicalNodeJSON extends NodeJSON {
+  id: string;
+  children: HierarchicalNodeJSON[];
+}
+
+export function toHierarchicalNodeJSONs(
+  nodes: Record<string, NodeJSON>
+): HierarchicalNodeJSON[] {
+  const hierarchicalNodes: Record<string, HierarchicalNodeJSON> = {};
+
+  for (const [id, node] of Object.entries(nodes)) {
+    hierarchicalNodes[id] = {
+      ...node,
+      id,
+      children: [],
+    };
+  }
+
+  for (const [id, node] of Object.entries(nodes)) {
+    if (node.parent) {
+      hierarchicalNodes[node.parent].children.push(hierarchicalNodes[id]);
+    }
+  }
+
+  for (const node of Object.values(hierarchicalNodes)) {
+    node.children.sort((a, b) => a.index - b.index);
+  }
+
+  return Object.values(hierarchicalNodes).filter((node) => !node.parent);
+}
--- packages/cli/src/project/WorkspaceLoader.ts
+++ packages/cli/src/project/WorkspaceLoader.ts
@@ -12,37 +12,10 @@ import { omit } from "lodash-es";
 import { formatJSON } from "../format";
 import { FileAccess } from "./FileAccess";
 import * as path from "path";
-
-interface HierarchicalNodeJSON extends NodeJSON {
-  id: string;
-  children: HierarchicalNodeJSON[];
-}
-
-function toHierarchicalNodeJSONs(
-  nodes: Record<string, NodeJSON>
-): HierarchicalNodeJSON[] {
-  const hierarchicalNodes: Record<string, HierarchicalNodeJSON> = {};
-
-  for (const [id, node] of Object.entries(nodes)) {
-    hierarchicalNodes[id] = {
-      ...node,
-      id,
-      children: [],
-    };
-  }
-
-  for (const [id, node] of Object.entries(nodes)) {
-    if (node.parent) {
-      hierarchicalNodes[node.parent].children.push(hierarchicalNodes[id]);
-    }
-  }
-
-  for (const node of Object.values(hierarchicalNodes)) {
-    node.children.sort((a, b) => a.index - b.index);
-  }
-
-  return Object.values(hierarchicalNodes).filter((node) => !node.parent);
-}
+import {
+  HierarchicalNodeJSON,
+  toHierarchicalNodeJSONs,
+} from "./HierarchicalNodeJSON";
 
 interface WorkspaceLoaderOptions {
   filePattern?: string;
