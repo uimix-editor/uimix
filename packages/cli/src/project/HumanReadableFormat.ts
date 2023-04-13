967ac5de455c98835174b8ebbede8e20cb41d6a1
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-13 15:07:17 +0900

    WIP

--- packages/cli/src/project/HumanReadableFormat.ts
+++ packages/cli/src/project/HumanReadableFormat.ts
@@ -1,4 +1,5 @@
 import { StyleJSON, VariantCondition } from "@uimix/model/src/data/v1";
+import { HierarchicalNodeJSON } from "./HierarchicalNodeJSON";
 
 type Variants = Record<string, Partial<StyleJSON>>;
 
@@ -7,7 +8,7 @@ type StyleProps = Partial<StyleJSON> & {
 };
 
 interface PageNode {
-  type: "node";
+  type: "page";
   id: string;
   children: (InstanceNode | FrameNode | LeafNode | ComponentNode)[];
 }
@@ -49,4 +50,82 @@ interface OverrideNode {
   type: "override";
   id: string;
   props: StyleProps;
+}
+
+type Node =
+  | PageNode
+  | ComponentNode
+  | VariantNode
+  | InstanceNode
+  | FrameNode
+  | LeafNode
+  | OverrideNode;
+
+export function toHumanReadableNode(json: HierarchicalNodeJSON): Node {
+  switch (json.type) {
+    case "project":
+      throw new Error("Project node is not supported in HumanReadableFormat");
+    case "page":
+      return {
+        type: json.type,
+        id: json.id,
+        children: json.children.map(
+          toHumanReadableNode
+        ) as PageNode["children"],
+      };
+    case "component": {
+      return {
+        type: json.type,
+        id: json.id,
+        children: json.children.map(
+          toHumanReadableNode
+        ) as ComponentNode["children"],
+      };
+    }
+    case "variant": {
+      return {
+        type: json.type,
+        props: {
+          condition: json.condition!, // TODO: skip if not present
+          // TODO: styles
+        },
+      };
+    }
+    case "text":
+    case "image":
+    case "svg":
+    case "foreign": {
+      return {
+        type: json.type,
+        id: json.id,
+        props: {
+          // TODO: styles
+        },
+      };
+    }
+    case "frame": {
+      return {
+        type: json.type,
+        id: json.id,
+        props: {
+          // TODO: styles
+        },
+        children: json.children.map(
+          toHumanReadableNode
+        ) as FrameNode["children"],
+      };
+    }
+    case "instance": {
+      return {
+        type: json.type,
+        id: json.id,
+        props: {
+          // TODO: styles
+        },
+        children: [
+          // TODO: overrides
+        ],
+      };
+    }
+  }
 }
