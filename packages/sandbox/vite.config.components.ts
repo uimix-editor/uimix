a10a6d6e1b05fbe3b493eebfe1c9ee0271ab1564
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-13 18:16:24 +0900

    WIP: use virtual module

--- packages/sandbox/vite.config.components.ts
+++ packages/sandbox/vite.config.components.ts
@@ -4,15 +4,37 @@ import * as path from "node:path";
 import * as url from "node:url";
 const __filename = url.fileURLToPath(import.meta.url);
 
+const entry = path.resolve(__filename, "../src/dummy.tsx");
+
+export function virtualModulePlugin() {
+  const virtualModuleId = "virtual:my-module";
+  const resolvedVirtualModuleId = "\0" + virtualModuleId;
+
+  return {
+    name: "my-plugin", // required, will show up in warnings and errors
+    resolveId(id) {
+      if (entry === id) {
+        return resolvedVirtualModuleId;
+      }
+    },
+    load(id) {
+      if (id === resolvedVirtualModuleId) {
+        return `export const msg = "from virtual module"`;
+      }
+    },
+    transform(src, id) {},
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // disable refresh of all components
   // otherwise "Error: @vitejs/plugin-react can't detect preamble. Something is wrong." is thrown
-  plugins: [react({ exclude: "**/*" })],
+  plugins: [virtualModulePlugin(), react({ exclude: "**/*" })],
 
   build: {
     lib: {
-      entry: path.resolve(__filename, "../src/uimix-components.tsx"),
+      entry: path.resolve(__filename, "../src/dummy.tsx"),
       name: "components",
       fileName: "components",
     },
