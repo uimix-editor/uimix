0054683e430e8fc26d9b4a894b45f0bcfa203fdc
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-17 01:13:19 +0900

    Use W3C design token

--- packages/sandbox/src/designTokens.ts
+++ packages/sandbox/src/designTokens.ts
@@ -1,47 +1,37 @@
-import { DesignToken } from "@uimix/code-asset-types";
 import colors from "tailwindcss/colors";
 
-function flattenColors(
-  colors: Record<string, string | Record<string, string>>
-): { name: string; value: string }[] {
-  const result: { name: string; value: string }[] = [];
+type DefaultColors = typeof colors;
 
-  for (const [name, value] of Object.entries(colors)) {
-    if (typeof value === "string") {
-      result.push({ name, value });
+interface ColorToken {
+  $value: string;
+  $type: "color";
+}
+
+type DesignToken = ColorToken;
+
+interface DesignTokens {
+  [key: string]: DesignToken | DesignTokens;
+}
+
+function colorsToDesignTokens(colors: DefaultColors): DesignTokens {
+  const designTokens: DesignTokens = {};
+
+  for (const [colorName, colorValue] of Object.entries(colors)) {
+    if (typeof colorValue === "string") {
+      designTokens[colorName] = {
+        $value: colorValue,
+        $type: "color",
+      };
     } else {
-      result.push(
-        ...flattenColors(value).map((v) => ({
-          name: `${name}/${v.name}`,
-          value: v.value,
-        }))
+      designTokens[colorName] = colorsToDesignTokens(
+        colorValue as DefaultColors
       );
     }
   }
 
-  return result;
+  return designTokens;
 }
 
-// TODO: support W3C design tokens?
-const tokens: DesignToken[] = [
-  // {
-  //   type: "color",
-  //   id: "@uimix/sandbox/accent",
-  //   name: "Accent",
-  //   value: "#1ea7fd",
-  // },
-  // {
-  //   type: "color",
-  //   id: "@uimix/sandbox/text",
-  //   name: "Text",
-  //   value: "#333333",
-  // },
-  ...flattenColors(colors).map(({ name, value }) => ({
-    type: "color",
-    id: name,
-    name,
-    value,
-  })),
-];
+const tokens = colorsToDesignTokens(colors);
 
 export default tokens;
