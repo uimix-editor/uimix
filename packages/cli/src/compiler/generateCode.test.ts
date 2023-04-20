6232394f0a1f93747c7f2e6e8ef12806cc04e6a1
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-21 01:02:38 +0900

    Improve test

--- packages/cli/src/compiler/generateCode.test.ts
+++ packages/cli/src/compiler/generateCode.test.ts
@@ -3,27 +3,23 @@ import { generateCode } from "./generateCode";
 import { WorkspaceLoader } from "../project/WorkspaceLoader";
 import * as path from "path";
 import { NodeFileAccess } from "../project/NodeFileAccess";
-// @ts-ignore
-import projectJSONFile from "../../../model/src/models/__fixtures__/project.uimixproject?raw";
-import { ProjectJSON } from "@uimix/model/src/data/v1";
 
 describe(generateCode.name, () => {
   it("generates code", async () => {
     const rootDir = path.resolve("../sandbox");
-    // TODO: use WorkspaceLoader
-    const projectJSON = ProjectJSON.parse(
-      JSON.parse(projectJSONFile as string)
+
+    const workspaceLoader = await WorkspaceLoader.load(
+      new NodeFileAccess(rootDir)
     );
+
+    // FIXME: you need to build code assets before generating code
+
     const code = await generateCode(
       rootDir,
-      {
-        components: {
-          react: ["./src/stories/*.tsx", "!./src/stories/*.stories.tsx"],
-        },
-        designTokens: "./src/designTokens.ts",
-      },
-      projectJSON,
-      new Map() // TODO
+      // TODO: better args
+      workspaceLoader.rootProject.manifest,
+      workspaceLoader.rootProject.project.toJSON(),
+      workspaceLoader.rootProject.imagePaths
     );
     const result: Record<string, string> = {};
     for (const file of code) {
