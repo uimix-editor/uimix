23cba4b93535185b78d2e0c3c5e5d299c3ec928e
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-20 16:42:41 +0900

    Use require for node:crypto

--- packages/foundation/src/utils/Crypto.ts
+++ packages/foundation/src/utils/Crypto.ts
@@ -1,2 +1,2 @@
-export const crypto =
-  globalThis.crypto ?? (await import("node:crypto")).webcrypto;
+// eslint-disable-next-line
+export const crypto = globalThis.crypto ?? require("node:crypto").webcrypto;
