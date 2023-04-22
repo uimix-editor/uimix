c3479d0da72126ddf7c0d3ae74b240a197896d13
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-22 17:07:32 +0900

    Cleanup

--- packages/model/src/models/PageList.ts
+++ packages/model/src/models/PageList.ts
@@ -5,7 +5,6 @@ import { compact } from "lodash-es";
 import { assertNonNull } from "@uimix/foundation/src/utils/Assert";
 import { Project } from "./Project";
 import { getPageID } from "../data/util";
-import { PathHierarchyFolder, PathHierarchy } from "./PathHierarchy";
 
 export class PageList {
   constructor(project: Project) {
@@ -32,55 +31,5 @@ export class PageList {
     this.node.append([node]);
     const page = assertNonNull(Page.from(node));
     return page;
-  }
-
-  toHierarchy(): PathHierarchyFolder<Page> {
-    return PathHierarchy.build(this.all);
-  }
-
-  delete(path: string): Page[] {
-    const deletedPages = PathHierarchy.targetsForPath(this.all, path);
-
-    for (const page of deletedPages) {
-      page.node.remove();
-      // TODO: delete dangling nodes?
-      //this.project.nodes.remove(doc.root);
-    }
-
-    return deletedPages;
-  }
-
-  rename(
-    path: string,
-    newPath: string
-  ): {
-    originalPages: Page[];
-    newPages: Page[];
-  } {
-    if (path === newPath) {
-      return {
-        originalPages: [],
-        newPages: [],
-      };
-    }
-
-    const originalPages = PathHierarchy.targetsForPath(this.all, path);
-    const newPages: Page[] = [];
-
-    for (const page of originalPages) {
-      const newName = newPath + page.filePath.slice(path.length);
-      const newPage = this.create(newName);
-      newPage.node.append(page.node.children);
-      newPages.push(newPage);
-    }
-
-    for (const page of originalPages) {
-      page.node.remove();
-    }
-
-    return {
-      originalPages,
-      newPages,
-    };
   }
 }
