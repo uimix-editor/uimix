04f7ddb7e35795c9b1b475bc3ec06279cf670d93
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-23 22:47:41 +0900

    WIP

---
+++ packages/adapter-react/src/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Component, ComponentRenderer, Prop } from "@uimix/adapter-types";
+
+export class ReactRenderer implements ComponentRenderer {
+  constructor(element: HTMLElement, Component: React.ElementType) {
+    this.reactRoot = ReactDOM.createRoot(element);
+    this.component = Component;
+  }
+
+  render(props: Record<string, unknown>) {
+    return new Promise<void>((resolve) => {
+      this.reactRoot.render(
+        <div style={{ display: "contents" }} ref={() => resolve()}>
+          <this.component {...props} />
+        </div>
+      );
+    });
+  }
+
+  dispose() {
+    this.reactRoot.unmount();
+  }
+
+  reactRoot: ReactDOM.Root;
+  component: React.ElementType;
+}
+
+export function reactComponent(options: {
+  path: string;
+  name?: string;
+  component: React.ElementType;
+  props: Record<string, Prop>;
+}): Component {
+  return {
+    framework: "react",
+    // relative to the package root (closest package.json)
+    path: options.path,
+    name: options.name ?? "default",
+    props: [
+      {
+        name: "primary",
+        type: { type: "boolean" },
+      },
+      {
+        name: "backgroundColor",
+        type: { type: "string" },
+      },
+      {
+        name: "size",
+        type: {
+          type: "enum",
+          values: ["small", "medium", "large"],
+        },
+      },
+      {
+        name: "label",
+        type: { type: "string" },
+      },
+    ],
+    createRenderer: (element: HTMLElement) =>
+      new ReactRenderer(element, options.component),
+  };
+}
+
+export * from "@uimix/adapter-types";
