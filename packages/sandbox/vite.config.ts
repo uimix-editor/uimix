beb9ab1f99ea923bed35682346937ade34c69cb0
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-24 10:37:25 +0900

    Fix vite config

--- packages/sandbox/vite.config.ts
+++ packages/sandbox/vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  define: {
-    // workaround for mui https://github.com/vitejs/vite/issues/9186#issuecomment-1189228653
-    "process.env.NODE_ENV": '"production"',
-  },
-});
+  ...(mode === "production"
+    ? {
+        define: {
+          // workaround for mui https://github.com/vitejs/vite/issues/9186#issuecomment-1189228653
+          "process.env.NODE_ENV": '"production"',
+        },
+      }
+    : {}),
+}));
