f1e410b017dd31806141b23ddf0552750b536f22
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-24 18:03:42 +0900

    Fix snapping

--- packages/editor/src/state/Snapper.ts
+++ packages/editor/src/state/Snapper.ts
@@ -73,7 +73,7 @@ export class Snapper {
 
     return [
       ...[...siblings].map((c) => c.computedRect),
-      parent.computedPaddingRect,
+      ...(!parent.originalNode.isAbstract ? [parent.computedPaddingRect] : []),
     ];
   }
 
