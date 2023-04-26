447ebd5e3fc5c18c6f18d00923c6564e5db3c629
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-26 22:06:07 +0900

    Add Text component

--- packages/elements-react/src/Box.stories.tsx
+++ packages/elements-react/src/Box.stories.tsx
@@ -1,4 +1,4 @@
-import { Box } from "./index";
+import { Box, Text } from "./index";
 
 export default {
   title: "Box",
@@ -7,4 +7,19 @@ export default {
 
 export const Basic = () => {
   return <Box width={100} height={200} fills={[{ solid: "#c0ffee" }]} />;
+};
+
+export const WithText = () => {
+  return (
+    <Box
+      width={100}
+      height={200}
+      fills={[{ solid: "#c0ffee" }]}
+      layout="flex"
+      flexDirection="y"
+    >
+      <Text fills={[{ solid: "#000" }]} textContent="Hello, world!" />
+      <Text fills={[{ solid: "#666" }]} textContent="This is a text" />
+    </Box>
+  );
 };
--- packages/elements-react/src/Box.tsx
+++ packages/elements-react/src/Box.tsx
@@ -39,3 +39,37 @@ export const Box: React.FC<
     </div>
   );
 };
+
+export const Text: React.FC<
+  Partial<StyleProps> & {
+    children?: ReactNode;
+  }
+> = (props) => {
+  const style = buildNodeCSS("text", {
+    ...defaultStyle,
+    ...props,
+  });
+
+  const id = useId().replaceAll(":", "_");
+  const className = `box-${id}`;
+
+  const cssBody = Object.entries(style.self)
+    .map(([key, value]) => {
+      return `${kebabCase(key)}: ${String(value)};`;
+    })
+    .join(";");
+  const childrenCSSBody = Object.entries(style.children)
+    .map(([key, value]) => {
+      return `${kebabCase(key)}: ${String(value)};`;
+    })
+    .join(";");
+
+  const styleText = `.${className}{${cssBody}} .${className}>*{${childrenCSSBody}}`;
+
+  return (
+    <div className={className}>
+      <style>{styleText}</style>
+      {props.textContent}
+    </div>
+  );
+};
