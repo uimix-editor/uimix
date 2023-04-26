ba907ad8947cfdd05372fc90b0edb196ce9c70aa
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-26 22:15:07 +0900

    Tweak example

--- packages/elements-react/src/Box.stories.tsx
+++ packages/elements-react/src/Box.stories.tsx
@@ -17,7 +17,11 @@ export const WithText = () => {
       fills={[{ solid: "#c0ffee" }]}
       layout="flex"
       flexDirection="y"
+      rowGap={20}
+      paddingLeft={10}
+      paddingRight={10}
     >
+      <Box width={100} height={100} fills={[{ solid: "#f0f0f0" }]} />
       <Text fills={[{ solid: "#000" }]} textContent="Hello, world!" />
       <Text fills={[{ solid: "#666" }]} textContent="This is a text" />
     </Box>
