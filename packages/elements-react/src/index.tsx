0735c0944ee790615ef78c8a1d904d1296e03825
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-26 13:54:26 +0900

    WIP

--- packages/elements-react/src/index.tsx
+++ packages/elements-react/src/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import { z } from "zod";
 
-export const Shadow = z.object({
+const Shadow = z.object({
   color: z.string(),
   x: z.number(),
   y: z.number(),
@@ -9,37 +9,32 @@ export const Shadow = z.object({
   spread: z.number(),
 });
 
-export type Shadow = z.infer<typeof Shadow>;
+type Shadow = z.infer<typeof Shadow>;
 
-export const StackDirection = z.enum(["x", "y"]);
-export type StackDirection = z.infer<typeof StackDirection>;
+const StackDirection = z.enum(["x", "y"]);
+type StackDirection = z.infer<typeof StackDirection>;
 
-export const StackAlign = z.enum(["start", "center", "end"]);
-export type StackAlign = z.infer<typeof StackAlign>;
+const StackAlign = z.enum(["start", "center", "end"]);
+type StackAlign = z.infer<typeof StackAlign>;
 
-export const StackJustify = z.enum(["start", "center", "end", "spaceBetween"]);
-export type StackJustify = z.infer<typeof StackJustify>;
+const StackJustify = z.enum(["start", "center", "end", "spaceBetween"]);
+type StackJustify = z.infer<typeof StackJustify>;
 
-export const TextHorizontalAlign = z.enum([
-  "start",
-  "center",
-  "end",
-  "justify",
-]);
-export type TextHorizontalAlign = z.infer<typeof TextHorizontalAlign>;
+const TextHorizontalAlign = z.enum(["start", "center", "end", "justify"]);
+type TextHorizontalAlign = z.infer<typeof TextHorizontalAlign>;
 
-export const TextVerticalAlign = z.enum(["start", "center", "end"]);
-export type TextVerticalAlign = z.infer<typeof TextVerticalAlign>;
+const TextVerticalAlign = z.enum(["start", "center", "end"]);
+type TextVerticalAlign = z.infer<typeof TextVerticalAlign>;
 
-export const Position = z.object({
+const Position = z.object({
   left: z.number().optional(),
   top: z.number().optional(),
   right: z.number().optional(),
   bottom: z.number().optional(),
 });
-export type Position = z.infer<typeof Position>;
+type Position = z.infer<typeof Position>;
 
-export const Size = z.union([
+const Size = z.union([
   z.literal("hug"),
   z.number(),
   z.object({
@@ -47,100 +42,82 @@ export const Size = z.union([
     max: z.number().optional(),
   }),
 ]);
-export type Size = z.infer<typeof Size>;
+type Size = z.infer<typeof Size>;
 
-export const Color = z.union([
+const Color = z.union([
   z.string(), // hex
   z.object({
     token: z.string(),
   }),
 ]);
-export type Color = z.infer<typeof Color>;
+type Color = z.infer<typeof Color>;
 
-export const Fill = z.object({
+const Fill = z.object({
   solid: Color,
 });
-export type Fill = z.infer<typeof Fill>;
-
-export const BaseStyleProps = z.object({
-  hidden: z.boolean(),
-  locked: z.boolean(),
-  position: Position,
-  absolute: z.boolean(),
-  width: Size,
-  height: Size,
-
-  topLeftRadius: z.number(),
-  topRightRadius: z.number(),
-  bottomRightRadius: z.number(),
-  bottomLeftRadius: z.number(),
-
-  fills: z.array(Fill),
-  border: z.union([Fill, z.null()]),
-  borderTopWidth: z.number(),
-  borderRightWidth: z.number(),
-  borderBottomWidth: z.number(),
-  borderLeftWidth: z.number(),
-
-  opacity: z.number(),
-  overflowHidden: z.boolean(),
-
-  shadows: z.array(Shadow),
-
-  // only for non-absolute layers
-  marginTop: z.number(),
-  marginRight: z.number(),
-  marginBottom: z.number(),
-  marginLeft: z.number(),
-
-  // layout
-
-  layout: z.enum(["none", "flex", "grid"]),
-  flexDirection: StackDirection,
-  flexAlign: StackAlign,
-  flexJustify: StackJustify,
-  gridRowCount: z.union([z.number(), z.null()]),
-  gridColumnCount: z.union([z.number(), z.null()]),
-  rowGap: z.number(),
-  columnGap: z.number(),
-  paddingTop: z.number(),
-  paddingRight: z.number(),
-  paddingBottom: z.number(),
-  paddingLeft: z.number(),
-
-  // text
-
-  textContent: z.string(),
-  fontFamily: z.string(),
-  fontWeight: z.number(),
-  fontSize: z.number(),
-  lineHeight: z.union([z.number(), z.string(), z.null()]), // percent means relative to font size (null for auto)
-  letterSpacing: z.union([z.number(), z.string()]), // percent means relative to font size: ;
-  textHorizontalAlign: TextHorizontalAlign,
-  textVerticalAlign: TextVerticalAlign,
-
-  // image
-  image: z.union([z.string(), z.null()]),
-
-  // svg
-  svg: z.string(),
-
-  // instance
-  component: z.union([z.string(), z.null()]),
-
-  // foreign
-  componentType: z.literal("react"),
-  props: z.record(z.unknown()),
-
-  // tag name
-  tagName: z.union([z.string(), z.null()]),
-});
-export type BaseStyleProps = z.infer<typeof BaseStyleProps>;
+type Fill = z.infer<typeof Fill>;
 
 export const Box: React.FC<
-  Partial<BaseStyleProps> & {
+  Partial<StyleProps> & {
     children?: ReactNode;
   }
 > = (props) => {
   return <div>{props.children}</div>;
 };
+
+interface StyleProps {
+  hidden: boolean;
+  position: Position;
+  absolute: boolean;
+  width: Size;
+  height: Size;
+  topLeftRadius: number;
+  topRightRadius: number;
+  bottomRightRadius: number;
+  bottomLeftRadius: number;
+  fills: Fill[];
+  border: Fill | null;
+  borderTopWidth: number;
+  borderRightWidth: number;
+  borderBottomWidth: number;
+  borderLeftWidth: number;
+  opacity: number;
+  overflowHidden: boolean;
+  shadows: Shadow[];
+
+  // only for non-absolute layers
+  marginTop: number;
+  marginRight: number;
+  marginBottom: number;
+  marginLeft: number;
+
+  // layout
+  layout: "none" | "flex" | "grid";
+  flexDirection: StackDirection;
+  flexAlign: StackAlign;
+  flexJustify: StackJustify;
+  gridRowCount: number | null;
+  gridColumnCount: number | null;
+  rowGap: number;
+  columnGap: number;
+  paddingTop: number;
+  paddingRight: number;
+  paddingBottom: number;
+  paddingLeft: number;
+
+  // text
+  textContent: string;
+  fontFamily: string;
+  fontWeight: number;
+  fontSize: number;
+  lineHeight: number | string | null; // percent means relative to font size (null for auto)
+  letterSpacing: number | string; // percent means relative to font size: ;
+  textHorizontalAlign: TextHorizontalAlign;
+  textVerticalAlign: TextVerticalAlign;
+
+  // image URL
+  image: string | null;
+
+  // tag name
+  tagName: string | null;
+}
