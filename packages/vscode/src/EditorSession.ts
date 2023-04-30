89354c7968fc3dac68ccd4944fe5578f59a5685f
Ryohei Ikegami <iofg2100@gmail.com>
2023-04-30 13:25:36 +0900

    Try: load editor JS directly

--- packages/vscode/src/EditorSession.ts
+++ packages/vscode/src/EditorSession.ts
@@ -162,6 +162,33 @@ export class EditorSession {
   }
 
   private getHTMLForWebview(webview: vscode.Webview): string {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script type="module">
+import RefreshRuntime from "http://localhost:5173/@react-refresh"
+RefreshRuntime.injectIntoGlobalHook(window)
+window.$RefreshReg$ = () => {}
+window.$RefreshSig$ = () => (type) => type
+window.__vite_plugin_react_preamble_installed__ = true
+</script>
+
+    <script type="module" src="http://localhost:5173/@vite/client"></script>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>UIMix Editor</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="http://localhost:5173/src/main.tsx"></script>
+  </body>
+</html>
+    `;
+  }
+  /*
+  private getHTMLForWebview(webview: vscode.Webview): string {
     const nonce = getNonce();
 
     const isDevelopment =
@@ -270,6 +297,7 @@ export class EditorSession {
       </html>
     `;
   }
+  */
 }
 
 function getNonce(): string {
